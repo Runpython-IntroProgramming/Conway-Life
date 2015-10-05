2bb0034b4a5ef4e0143372212a79c9ff67812b19
Eric Dennison <ericd@netdenizen.com>
2015-10-05 11:44:34 -0400

    Update conway.py

--- conway.py
+++ conway.py
@@ -1,1 +1,8 @@
 """
+conway.py
+Author: <your name here>
+Credit: <list sources used, if any>
+Assignment:
+Write and submit a program that plays Conway's Game of Life, per 
+https://github.com/HHS-IntroProgramming/Conway-Life
+"""
