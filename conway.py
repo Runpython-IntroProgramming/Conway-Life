918a50e45eaa5406fbf3de2f59b399b74d142a35
Morgan Meliment <morganmeliment@gmail.com>
2015-10-06 14:30:36 -0400

    Updated from Brython Server: October 6, 2015 2:31:03 PM EDT

--- conway.py
+++ conway.py
@@ -6,25 +6,25 @@ Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
-
 from ggame import App, Color, Sprite, RectangleAsset, LineStyle
 
-red = Color(0xff0000, 1.0)
-green = Color(0x00ff00, 1.0)
-blue = Color(0x0000ff, 1.0)
-brown = Color(0xd38d5f, 1.0)
-yellow = Color(0xffd42a, 1.0)
-black = Color(0x000000, 1.0)
 grey = Color(0x808080, 1.0)
 dark = Color(0x333333, 1.0)
 nostroke = LineStyle(0, black)
 
-pixel = RectangleAsset(5, 5, nostroke, grey)
+pixel = RectangleAsset(10, 10, nostroke, grey)
 
-def createline(y):
-    pixelline = [Sprite(pixel, (x, y)) for x in range(0, 960, 6)]
+field = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-[createline(b) for b in range(0, 480, 6)]
+def createfield(y):
+    pixelline = [Sprite(pixel, (x, y)) for x in range(0, 640, 8)]
+g = 0
+for b in field:
+    if b == 1:
+        x = (10 % field[g]) * 11
+        y = (field[g] / 10) * 11
+        Sprite(pixel, (x, y))
+    g += 1
 
 myapp = App()
 myapp.run()
