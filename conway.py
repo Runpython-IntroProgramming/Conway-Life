2d357c2e77d69b91ee7d466e91ccf526e62753ab
Morgan Meliment <morganmeliment@gmail.com>
2015-10-06 14:42:39 -0400

    Updated from Brython Server: October 6, 2015 2:43:06 PM EDT

--- conway.py
+++ conway.py
@@ -1,12 +1,13 @@
 """
 conway.py
 Author: Morgan Meliment
-Credit: http://brythonserver.github.io/ggame/#ggame.EllipseAsset
+Credit: http://brythonserver.github.io/ggame/#ggame.EllipseAsset, http://stackoverflow.com/questions/26454649/python-round-up-to-the-nearest-ten
 Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
 from ggame import App, Color, Sprite, RectangleAsset, LineStyle
+import math
 
 grey = Color(0x808080, 1.0)
 dark = Color(0x333333, 1.0)
@@ -14,17 +15,18 @@ nostroke = LineStyle(0, black)
 
 pixel = RectangleAsset(10, 10, nostroke, grey)
 
-field = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+field = []
+[field.append(1) for x in range(0, 30)]
 
-def createfield(y):
-    pixelline = [Sprite(pixel, (x, y)) for x in range(0, 640, 8)]
 g = 0
 for b in field:
-    if b == 1:
-        x = (10 % field[g]) * 11
-        y = (field[g] / 10) * 11
-        Sprite(pixel, (x, y))
     g += 1
+    if b == 1:
+        x = (g % 10) * 11
+        y = int(math.floor(g / 10.0)) * 11
+        Sprite(pixel, (x, y))
+        print(x , y)
+    
 
 myapp = App()
 myapp.run()
