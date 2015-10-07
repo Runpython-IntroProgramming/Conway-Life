1495873f2e9ee7e5c7d4f0fbcc871126810133e2
Morgan Meliment <morganmeliment@gmail.com>
2015-10-07 13:39:40 -0400

    Updated from Brython Server: October 7, 2015 1:40:08 PM EDT

--- conway.py
+++ conway.py
@@ -7,22 +7,17 @@ Write and submit a program that plays Conway's Game of Life, per
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
 from ggame import App, Color, Sprite, RectangleAsset, LineStyle
-import math
 
-grey = Color(0x808080, 0)
+
+grey = Color(0x808080, 1.0)
 dark = Color(0x808080, 1.0)
 nostroke = LineStyle(0, grey)
-
-field = []
-[field.append(1) for x in range(0, 1000)]
-
-pix = RectangleAsset(10,10,nostroke,grey)
-
-class Pixel(Sprite):
-    def __init__(self, width, height):
-        super().__init__(width, height)
+class pixel(Sprite):
+    pix = RectangleAsset(10,10,nostroke,grey)
+    def __init__(self, x, y):
+        Sprite(pix, (x, y))
     
-Pixel(560,430)
+pixel(40,49)
 
 
     
