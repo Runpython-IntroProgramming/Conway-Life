87334a5c0e48c11482df2a7a568b44ebad6b0f23
Morgan Meliment <morganmeliment@gmail.com>
2015-10-07 14:01:24 -0400

    Updated from Brython Server: October 7, 2015 2:01:53 PM EDT

--- conway.py
+++ conway.py
@@ -1,23 +1,29 @@
 """
 conway.py
 Author: Morgan Meliment
-Credit: http://brythonserver.github.io/ggame/#ggame.EllipseAsset, http://stackoverflow.com/questions/26454649/python-round-up-to-the-nearest-ten
+Credit: http://brythonserver.github.io/ggame/#ggame.EllipseAsset
 Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
-from ggame import App, Color, Sprite, RectangleAsset, LineStyle
+from ggame import App, Color, Sprite, RectangleAsset, LineStyle, MouseEvent
 
-
-grey = Color(0x808080, 1.0)
+grey = Color(0x808080, 0.6)
 dark = Color(0x808080, 1.0)
 nostroke = LineStyle(0, grey)
-class pixel(Sprite):
-    pix = RectangleAsset(10, 10, nostroke, grey)
-    def __init__(self, x, y):
-        Sprite(pixel.pix, (x, y))
+class Cell(Sprite):
     
-pixel(100,10)
+    pix = RectangleAsset(10, 10, nostroke, grey)
+    
+    def __init__(self, x, y):
+        Sprite(Cell.pix, (x, y))
+        click = True
+        if click == True:
+            self.color = dark
+        else:
+            self.color = grey
+    
+Cell(0, 15)
 
 
     
