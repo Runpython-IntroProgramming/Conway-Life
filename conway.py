d5c3c9d170947759cce63f0fe9983cea0d476199
Morgan Meliment <morganmeliment@gmail.com>
2015-10-12 13:48:15 -0400

    Updated from Brython Server: October 12, 2015 1:48:15 PM EDT

--- conway.py
+++ conway.py
@@ -5,7 +5,7 @@ Credit: http://brythonserver.github.io/ggame/#ggame.EllipseAsset
 Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
-"""
+
 from ggame import App, Color, Sprite, RectangleAsset, LineStyle, MouseEvent
 
 SCREEN_WIDTH = 640
@@ -81,7 +81,94 @@ class Conway(App):
 myapp = Conway(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
 
+"""
 
+from ggame import App, Color, Sprite, RectangleAsset, LineStyle, MouseEvent
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+grey = Color(0x808080, 1)
+nostroke = LineStyle(0, grey)
+livecells = {}
+newcells = []
+
+def makenewcells:
+    for pos in newcells:
+        neighbors = getcoor(posti[1], posti[0])
+        neighborcount = 0
+        for posi in neighbors:
+            if livecells.get(str((posi[1], posi[0])), False) == True:
+                neighborcount += 1
+        if neighborcount == 3:
+            Cell(pos)
+    
+
+def getcoor(xx, yy):
+    return([[yy - 10, xx - 10], [yy - 10, xx], [yy - 10, xx + 10], [yy, xx - 10], [yy, xx + 10], [yy + 10, xx - 10], [yy + 10, xx], [yy + 10, xx + 10]])
+
+def check(posti):
+    if livecells.get(str((posti[1], posti[0])), False) == False:
+        neig = getcoor(posti[1], posti[0])
+        neighborcoun = 0
+        for posi in neig:
+            if livecells.get(str((posi[1], posi[0])), False) == True:
+                neighborcoun += 1
+        if neighborcoun == 3:
+            Cell((posti[1], posti[0]))
+    return(True)
+    
+def find(xx, yy):
+    neighbors = getcoor(xx, yy)
+    neighborcount = 0
+    for posi in neighbors:
+        if livecells.get(str((posi[1], posi[0])), False) == True:
+            neighborcount += 1
+        else:
+            if str((posi[1], posi[0]) in newcells:
+            else:
+                newcells.append(str((posi[1], posi[0]))
+    return(neighborcount)
+
+class Cell(Sprite):
+    pix = RectangleAsset(10, 10, nostroke, grey)
+    
+    def __init__(self, position):
+        super().__init__(Cell.pix, position)
+        livecells[str(position)] = True
+        self.position = position
+        
+    def step(self):
+        n = find(self.x, self.y)
+        if n < 2:
+            livecells[str(self.position)] = False
+            self.destroy()
+        elif n >= 2 and n <= 3:
+        elif n > 3:
+            livecells[str(self.position)] = False
+            self.destroy()
+
+class Conway(App):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        Cell((0, 0))
+        Cell((10, 0))
+        Cell((0, 10))
+        Cell((10, 10))
+        Cell((20, 20))
+        Cell((30, 20))
+        Cell((20, 30))
+        Cell((30, 30))
+        
+    def step(self):
+        for cell in self.getSpritesbyClass(Cell):
+            cell.step()
+        makenewcells()
+    
+    
+
+myapp = Conway(SCREEN_WIDTH, SCREEN_HEIGHT)
+myapp.run()
 
 
 
