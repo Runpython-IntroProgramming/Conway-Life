0fe315cb46a4c7378b883257021c0a0a45be552b
Morgan Meliment <morganmeliment@gmail.com>
2015-10-12 15:03:53 -0400

    Updated from Brython Server: October 12, 2015 3:03:53 PM EDT

--- conway.py
+++ conway.py
@@ -91,32 +91,21 @@ SCREEN_HEIGHT = 480
 grey = Color(0x808080, 1)
 nostroke = LineStyle(0, grey)
 livecells = {}
-newcells = []
+makecells = []
+surcells = []
+killcells = []
 
-def makenewcells():
-    for pos in newcells:
-        neighbors = getcoor(posti[1], posti[0])
-        neighborcount = 0
-        for posi in neighbors:
-            if livecells.get(str((posi[1], posi[0])), False) == True:
-                neighborcount += 1
-        if neighborcount == 3:
-            Cell(pos)
-    
+def create():
+    for celle in makecells:
+        Cell(celle)
 
 def getcoor(xx, yy):
     return([[yy - 10, xx - 10], [yy - 10, xx], [yy - 10, xx + 10], [yy, xx - 10], [yy, xx + 10], [yy + 10, xx - 10], [yy + 10, xx], [yy + 10, xx + 10]])
 
-def check(posti):
-    if livecells.get(str((posti[1], posti[0])), False) == False:
-        neig = getcoor(posti[1], posti[0])
-        neighborcoun = 0
-        for posi in neig:
-            if livecells.get(str((posi[1], posi[0])), False) == True:
-                neighborcoun += 1
-        if neighborcoun == 3:
-            Cell(posti)
-    return(True)
+def check():
+    for celle in surcells:
+        if find(celle[1], celle[0]) == 3:
+            makecells.append((celle[1], celle[0]))
     
 def find(xx, yy):
     neighbors = getcoor(xx, yy)
@@ -124,11 +113,6 @@ def find(xx, yy):
     for posi in neighbors:
         if livecells.get(str((posi[1], posi[0])), False) == True:
             neighborcount += 1
-        else:
-            if posi in newcells:
-                print(newcells)
-            else:
-                newcells.append(posi)
     return(neighborcount)
 
 class Cell(Sprite):
@@ -140,15 +124,14 @@ class Cell(Sprite):
         self.position = position
         
     def step(self):
-        n = find(self.x, self.y)
-        if n < 2:
+        if self.position in killcells:
             livecells[str(self.position)] = False
             self.destroy()
-        elif n >= 2 and n <= 3:
-            print("Im alive")
-        elif n > 3:
-            livecells[str(self.position)] = False
-            self.destroy()
+        else:
+            n = find(self.x, self.y)
+            if n < 2 or n > 3:
+                killcells.append(self.position)
+            checking(self.x, self.y)
         
 
 class Conway(App):
@@ -157,13 +140,9 @@ class Conway(App):
         Cell((0, 0))
         Cell((10, 0))
         Cell((0, 10))
-        Cell((10, 10))
-        Cell((20, 20))
-        Cell((30, 20))
-        Cell((20, 30))
-        Cell((30, 30))
         
     def step(self):
+        create()
         for cell in self.getSpritesbyClass(Cell):
             cell.step()
         
