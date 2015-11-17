35c2ade42cd5666e84a1b25f1a1bfca1c415cd70
HHStudent <dimitrisomoff@hanovernorwichschools.org>
2015-11-17 14:56:14 -0500

    Updated from Brython Server: 11/17/2015 2:56:14 PM

--- conway.py
+++ conway.py
@@ -13,13 +13,21 @@ SCREEN_HEIGHT = 480
 CELLWIDTH = 64
 CELLHEIGHT = 48
 
-black = Color(0, 1)
+black = Color(0, .1)
 green = Color(0x00ff00, 1)
 white = Color(0xffffff, 1)
+blackline = LineStyle(.1, black)
 
 class Conways(App):
     def _init__(self, width, height):
-        super().__init__(width, height).fill(black)
+        super().__init__(width, height)
+        black = Color(0, .5)
+        blackline = LineStyle(1, black)
+        bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, blackline, black)
+        bg = Sprite(bg_asset, (0,0))
+        celloutline = RectangleAsset(CELLWIDTH, CELLHEIGHT, blackline, white)
+        for x in range(0, SCREEN_WIDTH, CELLWIDTH):
+            cell = Sprite(celloutline)
 
 
 
