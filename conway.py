7ecfd31ab0f6e0b102f8238340cdb126b6f087de
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-03-08 14:48:26 -0500

    Updated from Brython Server: 3/8/2016 2:49:46 PM

--- conway.py
+++ conway.py
@@ -1,8 +1,9 @@
 """
 conway.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Mary Feyrer
+Credit: Tess Snyder
 Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
+import grid
