6bd37d8f24fff0087f02a00dc3e8dc3749895991
tesssny <teresasnyder@hanovernorwichschools.org>
2016-03-09 13:45:49 -0500

    Updated from Brython Server: 3/9/2016 1:45:49 PM

--- conway.py
+++ conway.py
@@ -13,17 +13,20 @@ SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
 
-black = Color(0xFF0000, 1.0)
+black = Color(0x000000, 1.0)
 white = Color(0xFFFFFF, 1.0) #color of dead cell
 red = Color(0xff0000, 1.0) #color of first day of life
 blue = Color(0x0000ff, 1.0) #color of subsequent days of life
 
-noline = LineStyle(0, black)
+noline = LineStyle(.0001, black)
 
+square = RectangleAsset(2,2, noline, white)
 
 for x in range(0,50):
-    square = RectangleAsset(2,2, noline, white)
+    for y in range(0,50):
+        Sprite(square, (x,y))
 
 
-myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+myapp = App()
 myapp.run()
