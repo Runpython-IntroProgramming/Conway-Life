9d4ad79964212851d57b6d924e91581a1f385fcb
tesssny <teresasnyder@hanovernorwichschools.org>
2016-03-09 13:35:49 -0500

    Updated from Brython Server: 3/9/2016 1:35:49 PM

--- conway.py
+++ conway.py
@@ -12,11 +12,13 @@ from ggame import LineStyle, Color, Sprite, Sound
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
-noline = LineStyle(0, black)
 
+black = Color(0xFF0000, 1.0)
 white = Color(0xFFFFFF, 1.0) #color of dead cell
 red = Color(0xff0000, 1.0) #color of first day of life
 blue = Color(0x0000ff, 1.0) #color of subsequent days of life
+
+noline = LineStyle(0, black)
 
 myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
