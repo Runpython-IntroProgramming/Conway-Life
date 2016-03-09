d9b5c88b6b2eabcdc9520366de3902ed0d465b41
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-03-09 13:36:19 -0500

    Update conway.py

--- conway.py
+++ conway.py
@@ -6,4 +6,18 @@ Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
-import grid
+from ggame import App, RectangleAsset, ImageAsset, SoundAsset
+from ggame import LineStyle, Color, Sprite, Sound
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+red = Color(0xff0000, 1.0)
+noline = LineStyle(0, black)
+
+
+myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+myapp.run()
