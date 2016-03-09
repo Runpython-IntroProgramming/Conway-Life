ef6edec14c3f750e0901402c211bf7d74e7c5186
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-03-09 13:50:43 -0500

    Updated from Brython Server: 3/9/2016 1:52:05 PM

--- conway.py
+++ conway.py
@@ -13,12 +13,12 @@ white = Color(0xffffff, 1.0)
 blue = Color(0x000000, 1.0)
 black = Color(0x000000, 1.0)
 red = Color(0xff0000, 1.0)
-noline = LineStyle(.00001, black)
+noline = LineStyle(.1, black)
 
 square = RectangleAsset(10, 10, noline, black) 
 for x in range(0,10):
     for y in range(0,10):
-        Sprite(square, (10x,10y))
+        Sprite(square, (10*x,10*y))
     
 
 myapp = App()
