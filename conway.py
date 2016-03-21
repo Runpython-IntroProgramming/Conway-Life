7f4f513ec47731593a9bc69f111001f060560566
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-03-21 14:42:29 -0400

    Updated from Brython Server: 3/21/2016 2:42:29 PM

--- conway.py
+++ conway.py
@@ -14,10 +14,12 @@ blue = Color(0x000000, 1.0)
 black = Color(0x000000, 1.0)
 red = Color(0xff0000, 1.0)
 noline = LineStyle(0, black)
+line = LineStyle(.2, black)
 list1 = []
 
 square = RectangleAsset(20, 20, noline, red) 
-
+screen = RectangleAsset(400,400, line, white)
+Sprite(screen,(0,0))
 
 for x in range(0,20):
     for y in range(0,20):
