552b6e115171421a3679182347500449433dc747
tesssny <teresasnyder@hanovernorwichschools.org>
2016-03-21 14:14:28 -0400

    Updated from Brython Server: 3/21/2016 2:14:27 PM

--- conway.py
+++ conway.py
@@ -22,14 +22,14 @@ line = LineStyle(.1, black)
 
 list=[]
 
-square = RectangleAsset(15,15, line, white)
-Rsquare = RectangleAsset(15,15, line, red)
+square = RectangleAsset(20,20, line, white)
+Rsquare = RectangleAsset(20,20, line, red)
 
 
 for x in range(0,20):
     for y in range(0,20):
-        Sprite(square, (15*x,15*y))
-        list.append(Sprite(square, (15*x,15*y)))
+        Sprite(square, (20*x,20*y))
+        list.append(Sprite(square, (20*x,20*y)))
 
 
 
