affe718dea5cb75a511a882865b336a3f38a3101
glenpassow <glenpassow@hanovernorwichschools.org>
2016-03-24 14:17:51 -0400

    Updated from Brython Server: 3/24/2016 2:17:50 PM

--- conway.py
+++ conway.py
@@ -6,19 +6,18 @@ Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+
 red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 
-from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
-
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 1000
 
-rectangle = RectangleAsset(10, 10, thinline, blue)
-
 thinline = LineStyle(1, black)
+rectangle = RectangleAsset(10, 10, thinline, blue)
 
 a = 0
 b = 0
