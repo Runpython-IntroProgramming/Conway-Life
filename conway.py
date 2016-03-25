0c42cf66ca8e67b0d0b3920538fd8ab1607657e9
glenpassow <glenpassow@hanovernorwichschools.org>
2016-03-25 14:50:23 -0400

    Updated from Brython Server: 3/25/2016 2:50:22 PM

--- conway.py
+++ conway.py
@@ -12,21 +12,27 @@ red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
+white = Color(0xFFFFFF, 1.0)
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
 thinline = LineStyle(1, black)
-rectangle = RectangleAsset(20, 20, thinline, blue)
+rectangle = RectangleAsset(20, 20, thinline, white)
 
 a = 0
 b = 0
 
-for x in range(0, 24):
-    for x in range(0, 32):
-        Sprite(rectangle, (a, b))
+squares = {}
+
+for x in range(0, 11):
+    for y in range(0, 11):
+        squares[(x,y)] = Sprite(rectangle, (a, b))
         a = a+20
     a = 0
     b = b+20
+    
+
+
 myapp = App()
 myapp.run()
