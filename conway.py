bea0f5171471f61ad0faf9cc9402f87cd4786cd9
glenpassow <glenpassow@hanovernorwichschools.org>
2016-04-25 14:45:28 -0400

    Updated from Brython Server: April 25, 2016 2:45:28 PM EDT

--- conway.py
+++ conway.py
@@ -53,8 +53,8 @@ class ConwayGame(App):
         print("uhsdlhadlh")
         if self.going == False:
             print("its working?")
-            self.cx = ((self.cx/10)//1)*10
-            self.cy = ((self.cy/10)//1)*10
+            self.cx = (((self.cx/20)//1)*10)+10
+            self.cy = (((self.cy/20)//1)*10)+10
             squares[(self.cx, self.cy)].visible = False
             self.going = True
 
