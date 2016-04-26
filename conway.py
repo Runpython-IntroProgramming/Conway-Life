60edab55eedac702abf845222c6dc04e60f7ada5
glenpassow <glenpassow@hanovernorwichschools.org>
2016-04-26 14:18:33 -0400

    Updated from Brython Server: April 26, 2016 2:18:33 PM EDT

--- conway.py
+++ conway.py
@@ -50,18 +50,18 @@ class ConwayGame(App):
         self.going = False
 
     def step(self):
-        print("uhsdlhadlh")
         if self.going == False:
-            self.cx = int(self.cx/20)
-            self.cy = int(self.cy/20)
-            squares[(self.cx, self.cy)].visible = False
-            print("yay")
+            #self.cx = int(self.cx/20)
+            #self.cy = int(self.cy/20)
+            print(self.cx)
+            print(self.cy)
+            #squares[(self.cx, self.cy)].visible = False
+            squares[(1, 1)].visible = False
             self.going = True
            
 
 
         if self.going == True:
-            print("going now")    
             for g in range(0, height):
                 for f in range(0, width):
                     if squares[(g,f)].sca == True:
@@ -82,125 +82,3 @@ class ConwayGame(App):
                         squares[(s, d)].current = True
 myapp = ConwayGame()
 myapp.run()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-class ConwayGame(App):
-    
-    def __init__(self):
-        SCREEN_WIDTH = 640
-        SCREEN_HEIGHT = 480
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT)
-        
-        blue = Color(0x0000ff, 1.0)
-        black = Color(0x000000, 1.0)
-        green = Color(0x00ff00, 1.0)
-        thinline = LineStyle(1, black)
-        rectangle = RectangleAsset(20, 20, thinline, green)
-
-        a = 0
-        b = 0
-
-        squares = {}
-
-        height = 20
-        width = 20
-        
-        for x in range(0, height):
-            for y in range(0, width):
-                squares[(x,y)] = cell(rectangle, (a, b))
-                a = a+20
-                a = 0
-                b = b+20
-                
-        #def listenMouseEvent(cls, eventtype, callback)
-                
-        
-    def step(self):
-        for cell in self.getSpritesbyClass(cell):
-            cell.step()
-            for h in range(0, height):
-                for w in range(0, width):
-                    surroundingCellsAlive = 0
-                    #use just one of these and for loops and then use the variable instead of the +1's etc..
-                    if (w+1, h+1) in squares and squares[(w+1, h+1)].stateCurent == True:
-                        surroundingCellsAlive = surroundingCellsAlive + 1
-                    if ((w+1, h)) in squares and squares[(w+1, h)].stateCurent == True:
-                        surroundingCellsAlive = surroundingCellsAlive + 1
-                    if (w+1, h-1) in squares and squares[(w+1, h-1)].stateCurent == True:
-                        surroundingCellsAlive = surroundingCellsAlive + 1
-                    if (w, h-1) in squares and squares[(w, h-1)].stateCurent == True:
-                        surroundingCellsAlive = surroundingCellsAlive + 1
-                    if (w, h+1) in squares and squares[(w, h+1)].stateCurent == True:
-                        surroundingCellsAlive = surroundingCellsAlive + 1
-                    if (w-1, h-1) in squares and squares[(w-1, h-1)].stateCurent == True:
-                        surroundingCellsAlive = surroundingCellsAlive + 1
-                    if (w-1, h) in squares and squares[(w-1, h)].stateCurent == True:
-                        surroundingCellsAlive = surroundingCellsAlive + 1
-                    if (w-1, h+1) in squares and squares[(w-1, h+1)].stateCurent == True:
-                        surroundingCellsAlive = surroundingCellsAlive + 1
-                        
-                    if surroundingCellsAlive < 2:
-                        squares[(w, h)].nextState = False
-                    elif surroundingCellsAlive  == 2:
-                        squares[(w, h)].nextState = squares[(w, h)].stateCurrent
-                    elif surroundingCellsAlive == 3:
-                        squares[(w, h)].nextState = True
-                    else:
-                        squares[(w, h)].nextState = False
-                        
-                for h in range(0, height):
-                    for w in range(0, width):
-                        squares[(w, h)].stateCurent = squares[(w, h)].nextState
-
-myapp = ConwayGame()
-myapp.run()
-"""
