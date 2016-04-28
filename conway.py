df250d42bffcd815bd5b327a9987cc3feb8c1c4c
glenpassow <glenpassow@hanovernorwichschools.org>
2016-04-28 14:49:06 -0400

    Updated from Brython Server: April 28, 2016 2:49:06 PM EDT

--- conway.py
+++ conway.py
@@ -60,7 +60,6 @@ class ConwayGame(App):
                         for h in range(-1, 2):
                             if (w+g, h+f) in squares and squares[(w+g, h+f)].visible == True:
                                 squares[(g,f)].sca = squares[(g,f)].sca + 1
-                            print(squares[(g,f)].sca)
             
             for s in range(0, height):
                 for d in range(0, width):
