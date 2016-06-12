0b40fdcedcbae9048f90ac08565c30797ae3c338
adamglueck <adam.glueck1@gmail.com>
2016-06-11 21:22:33 -0400

    Update conway.py

--- conway.py
+++ conway.py
@@ -7,19 +7,20 @@ Write and submit a program that plays Conway's Game of Life, per
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
 from ggame import Frame, App, Color, RectangleAsset, Sprite, ImageAsset, LineStyle 
-
 black=Color(0x000000,1.0)
 celestegreen=Color(0x00FFB7,1.0)
 red=Color(0xBA0000,1.0)
 line=LineStyle(1, black)
-a=0
-b=0
+#imports graphics and defines colors and lines
 height=30
-width=30
-squares={}
+length=30
+#defines dimensions of cell
+dictionary={}
+#creates empty dictionary to store cell data
 thinline=LineStyle(1, black)
-livecell=RectangleAsset(30,30,line,celestegreen)
-deadcell=RectangleAsset(30,30,line,red)
+livingcell=RectangleAsset(30,30,line,celestegreen)
+zombiecell=RectangleAsset(30,30,line,red)
+#defines the look and dimensions of living cells and unliving cells :)
 
 class cell(Sprite):
     def __init__(self, asset, position):
@@ -28,45 +29,49 @@ class cell(Sprite):
         self.sca=0
 
 for x in range(0,height):
-            for y in range(0,width):
-                Sprite(deadcell,(x*height,y*width))
-                squares[(x,y)]=cell(livecell,(x*height,y*width))
+            for y in range(0,length):
+                Sprite(zombiecell,(x*height,y*length))
+                dictionary[(x,y)]=cell(livingcell,(x*height,y*length))
           
-class ConwayGame(App):
+class ConwayLife(App):
     
     def __init__(self):
-        ConwayGame.listenKeyEvent("keydown", "space",self.spaceclick)
-        SCREEN_WIDTH=960
+        ConwayLife.listenKeyEvent("keydown", "space",self.spaceclick)
+        #listens for click
+        SCREEN_length=960
         SCREEN_HEIGHT=720
-        self.going = False
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT)
-        ConwayGame.listenMouseEvent("click",self.create)
+        #sets display dimensions
+        self.moving = False
+        super().__init__(SCREEN_length, SCREEN_HEIGHT)
+        ConwayLife.listenMouseEvent("click",self.create)
     def create(self, event):
         self.cx=int(event.x/30)
         self.cy=int(event.y/30)
-        squares[(self.cx, self.cy)].visible=not squares[(self.cx, self.cy)].visible
+        dictionary[(self.cx, self.cy)].visible=not dictionary[(self.cx, self.cy)].visible
+        #this makes it so if you click on an undead cell, it becomes alive
     
     def spaceclick(self,event):
-        self.going=not self.going
+        self.moving=not self.moving
     def step(self):
-        if self.going==True:
-            for g in range(0,height):
-                for f in range(0,width):
-                    if squares[(g,f)].visible==True:
-                        squares[(g,f)].sca=squares[(g,f)].sca-1
-                    for w in range(-1,2):
-                        for h in range(-1,2):
-                            if (w+g, h+f) in squares and squares[(w+g,h+f)].visible==True:
-                                squares[(g,f)].sca=squares[(g,f)].sca+1
+        if self.moving==True:
+            for a in range(0,height):
+                for b in range(0,length):
+                    if dictionary[(a,b)].visible==True:
+                        #check to see if alive
+                     dictionary[(a,b)].sca dictionary[(a,b)].sca-1
+                    for c in range(-1,2):
+                        for d in range(-1,2):
+                            if (c+a, d+b) in dictionary and dictionary[(c+a,d+b)].visible==True:
+                             dictionary[(a,b)].sca dictionary[(a,b)].sca+1
             
-            for s in range(0,height):
-                for d in range(0,width):
-                    if squares[(s, d)].visible==True and squares[(s, d)].sca<2:
-                        squares[(s, d)].visible=False
-                    elif squares[(s, d)].visible==True and squares[(s, d)].sca>3:
-                        squares[(s, d)].visible=False
-                    elif squares[(s, d)].visible==False and squares[(s, d)].sca==3:
-                        squares[(s, d)].visible=True
-                    squares[(s,d)].sca=0
-myapp=ConwayGame()
+            for e in range(0,height):
+                for f in range(0,length):
+                    if dictionary[(e,f)].visible==True and dictionary[(e, f)].sca<2:
+                     dictionary[(e,f)].visible=False
+                    elif dictionary[(e,f)].visible==True and dictionary[(e, f)].sca>3:
+                     dictionary[(e,f)].visible=False
+                    elif dictionary[(e,f)].visible==False and dictionary[(e, f)].sca==3:
+                     dictionary[(e,f)].visible=True
+                 dictionary[(e,f)].sca=0
+myapp=ConwayLife()
 myapp.run()
