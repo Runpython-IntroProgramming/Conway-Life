941d10ebb1e2cfffbf8223037b714f3c9b3060e3
peterbynum <peterbynum@hanovernorwichschools.org>
2016-09-16 13:59:41 -0400

    Updated from Brython Server: September 16, 2016 1:59:40 PM EDT

--- conway.py
+++ conway.py
@@ -10,14 +10,22 @@ from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Fra
 
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 800
+black = Color(0, 1)
+green = Color(0x408000, 1.0)
+white = Color(0xffffff, 1.0)
+line = LineStyle(2, black)
 
-class Conway(app):
-    black = Color(0, 1)
-    green = Color(0x408000, 1.0)
-    white = Color(0xffffff, 1.0)
-    line = LineStyle(2, black)
-    bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, line, black)
+class Cell(Sprite):
+    asset = RectangleAsset(10,10, line, black)
+    def __init__(self, position):
+        super().__init__(Cell.asset, position)
+        
+
+class Conway(App):
+    bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, line, white)
     bg = Sprite(bg_asset, (0,0))
+    for row in range(0,121):
+        Cell((row*10,0))
 
 
 myapp = Conway(SCREEN_WIDTH, SCREEN_HEIGHT)
