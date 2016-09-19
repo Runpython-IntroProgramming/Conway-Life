c6bd1856c47e2950f140cf9de8feec71bc1ab75f
peterbynum <peterbynum@hanovernorwichschools.org>
2016-09-18 20:50:44 -0400

    Updated from Brython Server: 9/18/2016 8:50:43 PM

--- conway.py
+++ conway.py
@@ -8,8 +8,8 @@ https://github.com/HHS-IntroProgramming/Conway-Life
 """
 from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
 
-SCREEN_WIDTH = 1201
-SCREEN_HEIGHT = 800
+SCREEN_WIDTH = 1001
+SCREEN_HEIGHT = 600
 black = Color(0, 1)
 green = Color(0x408000, 1.0)
 white = Color(0xffffff, 1.0)
@@ -26,8 +26,8 @@ class Conway(App):
     bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, border, white)
     bg = Sprite(bg_asset, (0,0))
     for row in range(0,120):
-        for col in range(0,80):
-            Cell((row*10,col*10))
+        "for col in range(0,80):"
+        Cell((row*10,10))
 
 
 myapp = Conway(SCREEN_WIDTH, SCREEN_HEIGHT)
