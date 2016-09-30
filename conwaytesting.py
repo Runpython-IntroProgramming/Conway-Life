375ab29ccdfad286d41d83302e77094eb01b91dd
peterbynum <peterbynum@hanovernorwichschools.org>
2016-09-30 13:35:28 -0400

    Updated from Brython Server: 9/30/2016 1:35:28 PM

--- conwaytesting.py
+++ conwaytesting.py
@@ -17,31 +17,101 @@ border = LineStyle(2, black)
 line = LineStyle(1, black)
 
     
-spritelist = [[(0, 0, 0), (1, 1, 0), (0, 2, 0)], [(2, 0, 1), (0, 1, 1), (0, 2, 1)], [(0, 0, 2), (0, 1, 2), (0, 2, 2)]]
+spritelist = [[(0, 0, 0), (1, 1, 0), (0, 2, 0)], [(1, 0, 1), (0, 1, 1), (0, 2, 1)], [(0, 0, 2), (0, 1, 2), (0, 2, 2)]]
 print(spritelist)
-col = 1
-row = 1
+col = 0
+row = 0
 def countneighbors(col, row):
         s = 0
-        if col+1 <= 2 and row+1 <= 2 and col-1 >= 0 and row-1 >= 0:
-            if spritelist[col-1][row-1][0] == 1 or spritelist[col-1][row-1][0] == 2:
+        """if col+1 <= 2 and row+1 <= 2 and col-1 >= 0 and row-1 >= 0: # center
+            if spritelist[col-1][row-1][0] == 1 or spritelist[col-1][row-1][0] == 2: #7
                 s += 1
-            if spritelist[col-1][row][0] == 1 or spritelist[col-1][row][0] == 2:
+            if spritelist[col-1][row][0] == 1 or spritelist[col-1][row][0] == 2: #4
                 s += 1
-            if spritelist[col][row-1][0] == 1 or spritelist[col][row-1][0] == 2:
+            if spritelist[col][row-1][0] == 1 or spritelist[col][row-1][0] == 2: #8
                 s += 1
-            if spritelist[col-1][row+1][0] == 1 or spritelist[col-1][row+1][0] == 2:
+            if spritelist[col-1][row+1][0] == 1 or spritelist[col-1][row+1][0] == 2: #1
                 s += 1
-            if spritelist[col][row+1][0] == 1 or spritelist[col][row+1][0] == 2:
+            if spritelist[col][row+1][0] == 1 or spritelist[col][row+1][0] == 2: #2
                 s += 1
-            if spritelist[col+1][row-1][0] == 1 or spritelist[col+1][row-1][0] == 2:
+            if spritelist[col+1][row-1][0] == 1 or spritelist[col+1][row-1][0] == 2: #9
                 s += 1
-            if spritelist[col+1][row][0] == 1 or spritelist[col+1][row][0] == 2:
+            if spritelist[col+1][row][0] == 1 or spritelist[col+1][row][0] == 2: #6
                 s += 1
-            if spritelist[col+1][row+1][0] == 1 or spritelist[col+1][row+1][0] == 2:
+            if spritelist[col+1][row+1][0] == 1 or spritelist[col+1][row+1][0] == 2: #3
+                s += 1"""
+        if col+1 <= 2 and row+1 <= 2 and col-1 >=0 and row-1 < 0: #top middle
+            if spritelist[col-1][row+1][0] == 1 or spritelist[col-1][row+1][0] == 2: #1
                 s += 1
-        if col+1
-        if col+1 <= 2 and row+1 > 2:
+            if spritelist[col][row+1][0] == 1 or spritelist[col][row+1][0] == 2: #2
+                s += 1
+            if spritelist[col+1][row+1][0] == 1 or spritelist[col+1][row+1][0] == 2: #3
+                s += 1
+            if spritelist[col-1][row][0] == 1 or spritelist[col-1][row][0] == 2: #4
+                s += 1
+            if spritelist[col+1][row][0] == 1 or spritelist[col+1][row][0] == 2: #6
+                s += 1
+        elif col+1 <= 2 and row+1 <= 2 and col-1 < 0 and row-1 >= 0: #left middle
+            if spritelist[col][row+1][0] == 1 or spritelist[col][row+1][0] == 2: #2
+                s += 1
+            if spritelist[col+1][row+1][0] == 1 or spritelist[col+1][row+1][0] == 2: #3
+                s += 1
+            if spritelist[col+1][row][0] == 1 or spritelist[col+1][row][0] == 2: #6
+                s += 1
+            if spritelist[col][row-1][0] == 1 or spritelist[col][row-1][0] == 2: #8
+                s += 1
+            if spritelist[col+1][row-1][0] == 1 or spritelist[col+1][row-1][0] == 2: #9
+                s += 1
+        elif col+1 <= 2 and row+1 > 2 and col-1 >= 0 and row-1 >= 0: #bottom middle
+            if spritelist[col-1][row][0] == 1 or spritelist[col-1][row][0] == 2: #4
+                s += 1
+            if spritelist[col+1][row][0] == 1 or spritelist[col+1][row][0] == 2: #6
+                s += 1
+            if spritelist[col-1][row-1][0] == 1 or spritelist[col-1][row-1][0] == 2: #7
+                s += 1
+            if spritelist[col][row-1][0] == 1 or spritelist[col][row-1][0] == 2: #8
+                s += 1
+            if spritelist[col+1][row-1][0] == 1 or spritelist[col+1][row-1][0] == 2: #9
+                s += 1
+        elif col+1 >= 2 and row+1 <= 2 and col-1 >= 0 and row-1 >= 0: #right middle
+            if spritelist[col-1][row+1][0] == 1 or spritelist[col-1][row+1][0] == 2: #1
+                s += 1
+            if spritelist[col][row+1][0] == 1 or spritelist[col][row+1][0] == 2: #2
+                s += 1
+            if spritelist[col-1][row][0] == 1 or spritelist[col-1][row][0] == 2: #4
+                s += 1
+            if spritelist[col-1][row-1][0] == 1 or spritelist[col-1][row-1][0] == 2: #7
+                s += 1
+            if spritelist[col][row-1][0] == 1 or spritelist[col][row-1][0] == 2: #8
+                s += 1
+        elif col+1 <= 2 and row+1 <= 2 and col-1 < 0 and row-1 < 0: #left upper corner
+            if spritelist[col][row+1][0] == 1 or spritelist[col][row+1][0] == 2: #2
+                s += 1
+            if spritelist[col+1][row+1][0] == 1 or spritelist[col+1][row+1][0] == 2: #3
+                s += 1
+            if spritelist[col+1][row][0] == 1 or spritelist[col+1][row][0] == 2: #6
+                s += 1
+        elif col+1 > 2 and row+1 <= 2 and col-1 >= 0 and row-1 < 0: #right upper corner
+            if spritelist[col-1][row+1][0] == 1 or spritelist[col-1][row+1][0] == 2: #1
+                s += 1
+            if spritelist[col][row+1][0] == 1 or spritelist[col][row+1][0] == 2: #2
+                s += 1
+            if spritelist[col-1][row][0] == 1 or spritelist[col-1][row][0] == 2: #4
+                s += 1
+        elif col+1 > 2 and row+1 > 2 and col-1 >= 0 and row-1 >= 0: #right lower corner
+            if spritelist[col-1][row-1][0] == 1 or spritelist[col-1][row-1][0] == 2: #7
+                s += 1
+            if spritelist[col-1][row][0] == 1 or spritelist[col-1][row][0] == 2: #4
+                s += 1
+            if spritelist[col][row-1][0] == 1 or spritelist[col][row-1][0] == 2: #8
+                s += 1
+        elif col+1 <= 2 and row+1 > 2 and col-1 < 0 and row-1 >= 0: #left lower corner
+            if spritelist[col+1][row-1][0] == 1 or spritelist[col+1][row-1][0] == 2: #9
+                s += 1
+            if spritelist[col+1][row][0] == 1 or spritelist[col+1][row][0] == 2: #6
+                s += 1
+            if spritelist[col][row-1][0] == 1 or spritelist[col][row-1][0] == 2: #8
+                s += 1
         print(s)
         return s
 if countneighbors(2,1) == 2:
