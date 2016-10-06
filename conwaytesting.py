d0fcd466cb1b9f41763b2d5a50fbc96ce97dfac2
peterbynum <peterbynum@hanovernorwichschools.org>
2016-10-06 14:00:08 -0400

    Updated from Brython Server: 10/6/2016 2:00:10 PM

--- conwaytesting.py
+++ conwaytesting.py
@@ -17,7 +17,7 @@ border = LineStyle(2, black)
 line = LineStyle(1, black)
 mousex = 0
 mousey = 0
-createlife = False
+createlife = 0
 spritelist = 0
 
 class Cell(Sprite):
@@ -45,9 +45,13 @@ class Cell(Sprite):
             else:
                 spritelist[self.col][self.row][0] = 1
             
-        if createlife == True and self.mouseoverlife() == True:
+        if createlife == 1:
             print('hi')
             spritelist[self.col][self.row][0] = 2
+        
+        """if self.mouseoverlife == True:
+            print('hi')
+            spritelist[self.col][self.row][0] = 2"""
 
     def changecolor(self):
         if spritelist[self.col][self.row][0] == 0:
@@ -154,7 +158,6 @@ class Cell(Sprite):
                 s += 1
             if spritelist[self.col][self.row-1][0] == 1 or spritelist[self.col][self.row-1][0] == 2: #8
                 s += 1
-        print(s)
         return s
 
 
@@ -170,7 +173,7 @@ class Conway(App):
 
     def mousedown(self, event):
         global createlife
-        createlife = True
+        createlife = 1
 
     def mousemove(self, event):
         global mousex
@@ -180,14 +183,13 @@ class Conway(App):
 
     def mouseup(self, event):
         global createlife
-        createlife = False
+        createlife = 0
     
     def step(self):
         for g in self.getSpritesbyClass(Cell):
             g.step()
         for g in self.getSpritesbyClass(Cell):
             g.changecolor()
-
 
 myapp = Conway(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.listenMouseEvent('mousedown', myapp.mousedown)
