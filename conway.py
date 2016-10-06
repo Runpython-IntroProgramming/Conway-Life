bb9f157e259d99e7ea1846b8ad9bacabe490e1cd
T045T3R <johannestestorf@hanovernorwichschools.org>
2016-10-06 13:59:09 -0400

    Updated from Brython Server: 10/6/2016 1:59:12 PM

--- conway.py
+++ conway.py
@@ -1,8 +1,12 @@
 """
 conway.py
-Author: <your name here>
+Author: johannes testorf
 Credit: <list sources used, if any>
 Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
