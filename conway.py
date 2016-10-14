fa7e6b8ef8e43d905eb5119497a0f0411a2f73a1
T045T3R <johannestestorf@hanovernorwichschools.org>
2016-10-14 13:29:00 -0400

    Updated from Brython Server: 10/14/2016 1:29:04 PM

--- conway.py
+++ conway.py
@@ -33,7 +33,7 @@ for x in width:
 def mouseclick(event):
     pixelpositionx = ((event.x)//10)*10
     pixelpositiony = ((event.y)//10)*10
-    if rectangle == wsquareasset
+    if rectangle == wsquareasset:
         rectangle = PolygonAsset(((pixelpositionx*10, pixelpositiony*10), ((pixelpositionx+1)*10, pixelpositiony*10),((pixelpositionx+1)*10, (pixelpositiony+1)*10),((pixelpositionx)*10, (pixelpositiony+1)*10)), thinline, red)
         Sprite(rectangle)
 def spacekey(event):
