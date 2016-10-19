417bafc4bf8cdb8306931828587d850647c453cb
T045T3R <johannestestorf@hanovernorwichschools.org>
2016-10-18 22:46:53 -0400

    Updated from Brython Server: 10/18/2016 10:46:53 PM

--- conway.py
+++ conway.py
@@ -43,7 +43,7 @@ def mouseclick(event):
         colors['0'+str(pixelpositionx/10)+'0'+str(pixelpositiony/10)]=1
     if colors['0'+str(pixelpositionx/10)+'0'+str(pixelpositiony/10)]==1:
         Sprite(rsquare_asset, (pixelpositionx, pixelpositiony))
-        colors['0'+str(pixelpositionx/10)+'0'+str(pixelpositiony/10)]=0
+        colors['0'+str(int(pixelpositionx/10))+'0'+str(int(pixelpositiony/10))]=0
     
 def doubleclick(event):
     pixelpositionx = ((event.x)//10)*10
