f373c50527fe707200387690c8ad50980a7f3407
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-02-08 14:48:45 -0500

    Updated from Brython Server: 2/8/2017 2:48:44 PM

--- conway.py
+++ conway.py
@@ -18,19 +18,30 @@ gray = Color(0x8c8c8c, 1)
 # Background
 
 noline = LineStyle(0, black)
+thinline = LineStyle(1, black)
 bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, white)
 bg = Sprite(bg_asset, (0,0))
 
 
-cellframe = RectangleAsset(20, 30, noline, gray)
-cf1 = Sprite(cellframe)
+cf = RectangleAsset(20, 30, thinline, gray)
 
-celllist = list(range(1, 496))
+
+celllist = list(range(1, 497))
 i = 0
 x = 0
 y = 0
-while i < 1:
-    
+c = 0
+while i != 1:
+    celllist[c] = Sprite(cf, (x, y))
+    if y > (SCREEN_HEIGHT - 30):
+        i=1
+    elif x > (SCREEN_WIDTH - 20):
+        x=0
+        y=y+30
+        c = c+1
+    else:
+        x=x+20
+        c= c+1
 
 
 myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
