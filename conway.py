c2e98cc63570dcd1096b32d46a459395f414554a
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-03-01 12:19:40 -0500

    Updated from Brython Server: 3/1/2017 12:19:39 PM

--- conway.py
+++ conway.py
@@ -46,36 +46,34 @@ class Cell(Sprite):
         self.s = state
         self.l = int(listposition)
     def step(self):
-        alive = 0
-        if celllist[self.l-32].s in [1, 2]:
-            alive += 1
-        if celllist[self.l-33].s in [1, 2]:
-            alive += 1
-            print("True")
-        if celllist[self.l-34].s in [1, 2]:
-            alive += 1
-        if celllist[self.l-1].s in [1, 2]:
-            alive += 1
-        if celllist[self.l+1].s in [1, 2]:
-            alive += 1
-        if celllist[self.l+32].s in [1, 2]:
-            alive += 1
-        if celllist[self.l+33].s in [1, 2]:
-            alive += 1
-        if celllist[self.l+34].s in [1, 2]:
-            alive += 1
-        else:
-            alive = alive
-        if alive>=4 or alive<=1:
-            self.s = 0
-            print(self.s)
-            print("Yay")
-        elif self.s==2:
-            self.s=2
-        else:
-            self.s += 1
-            print(celllist[77].s)
-            print(celllist[42].s)
+        checked = False
+        if checked == False:
+            checked = True
+            alive = 0
+            if celllist[self.l-32].s in [1, 2]:
+                alive += 1
+            if celllist[self.l-33].s in [1, 2]:
+                alive += 1
+            if celllist[self.l-34].s in [1, 2]:
+                alive += 1
+            if celllist[self.l-1].s in [1, 2]:
+                alive += 1
+            if celllist[self.l+1].s in [1, 2]:
+                alive += 1
+            if celllist[self.l+32].s in [1, 2]:
+                alive += 1
+            if celllist[self.l+33].s in [1, 2]:
+                alive += 1
+            if celllist[self.l+34].s in [1, 2]:
+                alive += 1
+            else:
+                alive = alive
+            if alive>=4 or alive<=1:
+                self.s = 0
+            elif self.s==2:
+                self.s=2
+            else:
+                self.s += 1
     
 celllist =list(range(1, 563))
 i = 0
@@ -98,6 +96,8 @@ while i != 1:
 
 celllist[43].s=1
 celllist[42].s=1
+celllist[77].s=1
+
 alivelist=[celllist[43], celllist[44], celllist[77]]
 print(celllist[43].s)
 
