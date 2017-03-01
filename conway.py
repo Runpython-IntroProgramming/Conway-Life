d959f66799fc791b1f6eba88f5c6859693a11024
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-03-01 15:43:40 -0500

    Updated from Brython Server: 3/1/2017 3:43:39 PM

--- conway.py
+++ conway.py
@@ -43,7 +43,7 @@ cf = RectangleAsset(20, 30, thinline, gray)
 #Cell Class
 class Cell(Sprite):
     #self, url, frame=None, qty=1, direction='horizontal', margin=0)
-    asset = ImageAsset("Animationonway.png", Frame(50, 10, 50-70, 10-40), 3, vertical)
+    asset = ImageAsset("Animationonway.png", Frame(50, 10, 50-70, 10-40), 3, 'vertical')
     def __init__(self, position, listposition, state, step):
         
         super().__init__(asset, position)
