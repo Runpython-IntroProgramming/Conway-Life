fc44f98243a1c9641d949ae5c8fcc139ce185134
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-03-01 19:25:56 -0500

    Updated from Brython Server: 3/1/2017 7:25:56 PM

--- conway.py
+++ conway.py
@@ -57,8 +57,160 @@ class Cell(Sprite):
         if self.checked == 0:
             self.checked = 1
     def alive1(self):
-        if:
-            
+        if self.l+36>len(celllist):
+            if self.l+37>len(celllist):
+                if self.l+38>len(celllist):
+                    if self.l+1>len(celllist):
+                        surround = [celllist[self.l-37], celllist[self.l-36], celllist[self.l-38], celllist[(self.l+36)-704], celllist[(self.l+37)-704], celllist[(self.l+38)-704], celllist[self.l-1], celllist[(self.l+1])-704]
+                        if celllist[self.l-36].s in [1,2]:
+                            alive += 1
+                        if celllist[self.l-37].s in [1,2]:
+                            alive += 1
+                        if celllist[self.l-38].s in [1,2]:
+                            alive += 1
+                        if celllist[self.l-1].s in [1,2]:
+                            alive += 1
+                        if celllist[(self.l+1)-704].s in [1,2]:
+                            alive += 1
+                        if celllist[(self.l+36)-704].s in [1,2]:
+                            alive += 1
+                        if celllist[(self.l+37)-704].s in [1,2]:
+                            alive += 1
+                        if celllist[(self.l+38)-704].s in [1,2]:
+                            alive += 1   
+                    else:
+                        surround = [celllist[self.l-37], celllist[self.l-36], celllist[self.l-38], celllist[(self.l+36)-704], celllist[(self.l+37)-704], celllist[(self.l+38)-704], celllist[self.l-1], celllist[(self.l+1])]
+
+                        if celllist[self.l-36].s in [1,2]:
+                            alive += 1
+                        if celllist[self.l-37].s in [1,2]:
+                            alive += 1
+                        if celllist[self.l-38].s in [1,2]:
+                            alive += 1
+                        if celllist[self.l-1].s in [1,2]:
+                            alive += 1
+                        if celllist[self.l+1].s in [1,2]:
+                            alive += 1
+                        if celllist[(self.l+36)-704].s in [1,2]:
+                            alive += 1
+                        if celllist[(self.l+37)-704].s in [1,2]:
+                            alive += 1
+                        if celllist[(self.l+38)-704].s in [1,2]:
+                            alive += 1
+                else:
+                    surround = [celllist[self.l-37], celllist[self.l-36], celllist[self.l-38], celllist[(self.l+36)-704], celllist[(self.l+37)-704], celllist[(self.l+38)], celllist[self.l-1], celllist[(self.l+1])]
+                    if celllist[self.l-36].s in [1,2]:
+                        alive += 1
+                    if celllist[self.l-37].s in [1,2]:
+                        alive += 1
+                    if celllist[self.l-38].s in [1,2]:
+                        alive += 1
+                    if celllist[self.l-1].s in [1,2]:
+                        alive += 1
+                    if celllist[self.l+1].s in [1,2]:
+                        alive += 1
+                    if celllist[(self.l+36)-704].s in [1,2]:
+                        alive += 1
+                    if celllist[(self.l+37)-704].s in [1,2]:
+                        alive += 1
+                    if celllist[(self.l+38)].s in [1,2]:
+                        alive +=1
+            else:
+                surround = [celllist[self.l-37], celllist[self.l-36], celllist[self.l-38], celllist[(self.l+36)-704], celllist[(self.l+37)], celllist[(self.l+38)], celllist[self.l-1], celllist[(self.l+1])]
+                if celllist[self.l-36].s in [1,2]:
+                        alive += 1
+                if celllist[self.l-37].s in [1,2]:
+                        alive += 1
+                if celllist[self.l-38].s in [1,2]:
+                        alive += 1
+                if celllist[self.l-1].s in [1,2]:
+                        alive += 1
+                if celllist[self.l+1].s in [1,2]:
+                        alive += 1
+                if celllist[(self.l+36)-704].s in [1,2]:
+                        alive += 1
+                if celllist[(self.l+37)].s in [1,2]:
+                        alive += 1
+                if celllist[(self.l+38)].s in [1,2]:
+                        alive +=1
+        elif self.1 - 36 < len(celllist):
+            if self.1 - 37 < len(celllist):
+                self.1 - 38 < len(celllist):
+                    if self.l-1 < len(celllist):
+                        surround = [(celllist[self.l-37)+704], celllist[(self.l-36)+704], celllist[(self.l-38)+704], celllist[(self.l+36)], celllist[(self.l+37)], celllist[(self.l+38)], celllist[(self.l-1)+704], celllist[(self.l+1])]
+                        if celllist[(self.l-36)+704].s in [1,2]:
+                            alive += 1
+                        if celllist[(self.l-37)+704].s in [1,2]:
+                            alive += 1
+                        if celllist[(self.l-38)+704].s in [1,2]:
+                            alive += 1
+                        if celllist[(self.l-1)+704].s in [1,2]:
+                            alive += 1
+                        if celllist[self.l+1].s in [1,2]:
+                            alive += 1
+                        if celllist[self.l+36].s in [1,2]:
+                            alive += 1
+                        if celllist[self.l+37].s in [1,2]:
+                            alive += 1
+                        if celllist[self.l+38].s in [1,2]:
+                            alive += 1
+                    else:
+                        surround = [(celllist[self.l-37)+704], celllist[(self.l-36)+704], celllist[(self.l-38)+704], celllist[(self.l+36)], celllist[(self.l+37)], celllist[(self.l+38)], celllist[(self.l-1)], celllist[(self.l+1])]
+                        if celllist[(self.l-36)+704].s in [1,2]:
+                            alive += 1
+                        if celllist[(self.l-37)+704].s in [1,2]:
+                            alive += 1
+                        if celllist[(self.l-38)+704].s in [1,2]:
+                            alive += 1
+                        if celllist[self.l-1].s in [1,2]:
+                            alive += 1
+                        if celllist[self.l+1].s in [1,2]:
+                            alive += 1
+                        if celllist[self.l+36].s in [1,2]:
+                            alive += 1
+                        if celllist[self.l+37].s in [1,2]:
+                            alive += 1
+                        if celllist[self.l+38].s in [1,2]:
+                            alive += 1
+                else:
+                    surround = [(celllist[self.l-37)+704], celllist[(self.l-36)+704], celllist[(self.l-38)], celllist[(self.l+36)], celllist[(self.l+37)], celllist[(self.l+38)], celllist[(self.l-1)], celllist[(self.l+1])]
+                    if celllist[(self.l-36)+704].s in [1,2]:
+                        alive += 1
+                    if celllist[(self.l-37)+704].s in [1,2]:
+                        alive += 1
+                    if celllist[(self.l-38)].s in [1,2]:
+                        alive += 1
+                    if celllist[self.l-1].s in [1,2]:
+                        alive += 1
+                    if celllist[self.l+1].s in [1,2]:
+                        alive += 1
+                    if celllist[self.l+36].s in [1,2]:
+                        alive += 1
+                    if celllist[self.l+37].s in [1,2]:
+                        alive += 1
+                    if celllist[self.l+38].s in [1,2]:
+                            alive += 1
+            else:
+                surround = [(celllist[self.l-37)], celllist[(self.l-36)+704], celllist[(self.l-38)], celllist[(self.l+36)], celllist[(self.l+37)], celllist[(self.l+38)], celllist[(self.l-1)], celllist[(self.l+1])]
+                if celllist[(self.l-36)+704].s in [1,2]:
+                        alive += 1
+                    if celllist[(self.l-37)].s in [1,2]:
+                        alive += 1
+                    if celllist[(self.l-38)].s in [1,2]:
+                        alive += 1
+                    if celllist[self.l-1].s in [1,2]:
+                        alive += 1
+                    if celllist[self.l+1].s in [1,2]:
+                        alive += 1
+                    if celllist[self.l+36].s in [1,2]:
+                        alive += 1
+                    if celllist[self.l+37].s in [1,2]:
+                        alive += 1
+                    if celllist[self.l+38].s in [1,2]:
+                            alive += 1
+        else: 
+            self.alive()
+        self.check()
         
     def alive(self):
         surround = [celllist[self.l-37], celllist[self.l-36], celllist[self.l-38], celllist[self.l+36], celllist[self.l+37], celllist[self.l+38], celllist[self.l-1], celllist[self.l+1]]
@@ -148,8 +300,6 @@ def mouseClick(event):
                 x.setImage(0)
                 if celllist[x.l] in alivelist:
                     alivelist.remove(celllist[x.l])
-            
-    
 myapp.listenKeyEvent('keydown', 'space', spaceKey)
 myapp.listenMouseEvent('click', mouseClick)
 # generations
