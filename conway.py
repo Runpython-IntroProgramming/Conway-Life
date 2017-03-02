f599dc84acabc79aaa648d29bbc26656401708f6
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-03-01 19:30:50 -0500

    Updated from Brython Server: 3/1/2017 7:30:50 PM

--- conway.py
+++ conway.py
@@ -194,20 +194,20 @@ class Cell(Sprite):
                 surround = [celllist[(self.l-37)], celllist[(self.l-36)+704], celllist[(self.l-38)], celllist[(self.l+36)], celllist[(self.l+37)], celllist[(self.l+38)], celllist[(self.l-1)], celllist[(self.l+1)]]
                 if celllist[(self.l-36)+704].s in [1,2]:
                         alive += 1
-                    if celllist[(self.l-37)].s in [1,2]:
-                        alive += 1
-                    if celllist[(self.l-38)].s in [1,2]:
-                        alive += 1
-                    if celllist[self.l-1].s in [1,2]:
-                        alive += 1
-                    if celllist[self.l+1].s in [1,2]:
-                        alive += 1
-                    if celllist[self.l+36].s in [1,2]:
-                        alive += 1
-                    if celllist[self.l+37].s in [1,2]:
-                        alive += 1
-                    if celllist[self.l+38].s in [1,2]:
-                            alive += 1
+                if celllist[(self.l-37)].s in [1,2]:
+                    alive += 1
+                if celllist[(self.l-38)].s in [1,2]:
+                    alive += 1
+                if celllist[self.l-1].s in [1,2]:
+                    alive += 1
+                if celllist[self.l+1].s in [1,2]:
+                    alive += 1
+                if celllist[self.l+36].s in [1,2]:
+                    alive += 1
+                if celllist[self.l+37].s in [1,2]:
+                    alive += 1
+                if celllist[self.l+38].s in [1,2]:
+                    alive += 1
         else: 
             self.alive()
         self.check()
