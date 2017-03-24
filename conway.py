35f305349dd7bbcd1448d64a8be9c830be689102
afeyrer <abigailfeyrer@hanovernorwichschools.org>
2017-03-24 14:45:33 -0400

    Updated from Brython Server: 3/24/2017 2:45:32 PM

--- conway.py
+++ conway.py
@@ -10,12 +10,10 @@ https://github.com/HHS-IntroProgramming/Conway-Life
 from ggame import App, RectangleAsset, ImageAsset, SoundAsset
 from ggame import LineStyle, Color, Sprite, Sound
 import time
-xs=list(range(0,10))
-ys=list(range(0,8))
 
 x=1
-SCREEN_WIDTH = 100
-SCREEN_HEIGHT = 80
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
 
 blue=Color(0x87cefa, 1)
 purple=Color(0x7b68ee, 1)
@@ -23,13 +21,13 @@ line=LineStyle(0,blue)
 white = Color(0xfffafa,1)
 green = Color(0x00ff00, 1)
 black = Color(0, 1)
-bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, line, white)
+bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, line, green)
 bg = Sprite(bg_asset, (0,0))
 
 tens=lambda x: round(x,-1)
 
 class notcell(Sprite):
-    asset=RectangleAsset(11,11,line,green)
+    asset=RectangleAsset(11,11,line,white)
     def __init__(self, position):
         super().__init__(notcell.asset, position)
 
@@ -49,4 +47,4 @@ for x in xs:
 
 myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
-myapp.listenMouseEvent('click', mouseClick)
+myapp.listenMouseEvent('clickdown', mouseClick)
