ed7b2f0f910455d12b59d5d5656c4e6f27487ead
Npikielny <42814781+Npikielny@users.noreply.github.com>
2018-09-10 12:07:51 -0400

    Updated from Brython Server: 9/10/2018 12:07:50 PM

--- conway.py
+++ conway.py
@@ -1,8 +1,12 @@
 """
 conway.py
-Author: <your name here>
+Author: Noah Pikielny
 Credit: <list sources used, if any>
 Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
+#Any live cell with fewer than two live neighbors dies, as if by under population.
+#Any live cell with two or three live neighbors lives on to the next generation.
+#Any live cell with more than three live neighbors dies, as if by overpopulation.
+#Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
