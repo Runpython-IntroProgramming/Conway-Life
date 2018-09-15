702f2eda52077d925085dcc89cda7d983791e91d
Npikielny <42814781+Npikielny@users.noreply.github.com>
2018-09-15 11:34:12 -0400

    Updated from Brython Server: 9/15/2018 11:34:12 AM

--- conway.py
+++ conway.py
@@ -11,10 +11,11 @@ https://github.com/HHS-IntroProgramming/Conway-Life
 #Any live cell with more than three live neighbors dies, as if by overpopulation.
 #Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
 from ggame import App, Color, LineStyle, Sprite, CircleAsset, Frame, RectangleAsset
-
+from random import randint
 red = Color(0xff0000, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
+green  = Color(0x696969, 1.0)
 white = Color(0xffffff, 1.0)
 noLine  = LineStyle(0, black)
 outLine = LineStyle(1, white)
@@ -23,11 +24,14 @@ frameWidth = 800
 frameHeight = 800
 cellSide = 5
 cells = {}
-for i in range(0, 20):
-    for k in range(0, 20):
-        cells[(k * 10,i * 10)] = "zombieCell"
-print(cells)
-"""
+for i in range(0, 50):
+    for k in range(0, 50):
+        if randint(0,5) == 4:
+            cells[(k * 10,i * 10)] = "alive"
+        else:
+            cells[(k * 10,i * 10)] = "dead"
+
+
 class GameOfLife(App):
     
     def __init__(self, width, height):
@@ -36,16 +40,18 @@ class GameOfLife(App):
         Sprite(bg, (0,0))
         #cell = CircleAsset(10, noLine, blue)
         for l in cells.keys():
-            cell(l)
+            cell(l,cells[l])
     
 
 class cell(Sprite):
-    deadCell = RectangleAsset(10,10, outLine, blue)
-    liveCell = CircleAsset(5, outLine, blue)
+    deadCell = RectangleAsset(10,10, outLine, black)
+    liveCell = CircleAsset(5, outLine, green)
 
-    def __init__(self, position):
-        super().__init__(cell.deadCell, position)
+    def __init__(self, position, state):
+        if state == "alive":
+            super().__init__(cell.liveCell, position)
+        else:
+            super().__init__(cell.deadCell, position)
 
 myapp = GameOfLife(frameWidth, frameHeight)
 myapp.run()
-"""
