3f382db183ec8449b9fed625bec84de2554ec643
emmadunbar <42850826+emmadunbar@users.noreply.github.com>
2018-09-17 12:37:37 -0400

    Updated from Brython Server: 9/17/2018 12:37:36 PM

--- conway.py
+++ conway.py
@@ -6,3 +6,18 @@ Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
+from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
+
+green=Color(0x00ff00, 1.0)
+blue=Color(0x0000ff, 1.0)
+black=Color(0x000000, 1.0)
+line=LineStyle(1, black)
+
+life1=(RectangleAsset(3,3,line,green))
+life2=(RectangleAsset,(3,3,line,blue))
+
+Sprite(life1,(30,30))
+Sprite(life2,(40,40))
+
+myapp = App()
+myapp.run()
