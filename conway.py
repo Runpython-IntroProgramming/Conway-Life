22e3426d5f33b4db87c0bde5caad7ffaa7811812
emmadunbar <42850826+emmadunbar@users.noreply.github.com>
2018-09-18 13:30:45 -0400

    Updated from Brython Server: 9/18/2018 1:30:44 PM

--- conway.py
+++ conway.py
@@ -11,10 +11,10 @@ from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, El
 green=Color(0x00ff00, 1.0)
 blue=Color(0x0000ff, 1.0)
 black=Color(0x000000, 1.0)
-tline=LineStyle(1, black)
+tline=LineStyle(0, black)
 
-life1=(RectangleAsset(3, 3, tline, green))
-life2=(RectangleAsset(3, 3, tline, blue))
+life1=(RectangleAsset(2, 2, tline, green))
+life2=(RectangleAsset(2, 2, tline, blue))
 
 #infinity=float'inf'
 
