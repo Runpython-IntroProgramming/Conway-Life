ab93cb6d2db6bddf810898ca8c10309809b8c9cf
emmatysinger <42844450+emmatysinger@users.noreply.github.com>
2018-09-19 12:12:29 -0400

    Updated from Brython Server: 9/19/2018 12:12:25 PM

--- conway.py
+++ conway.py
@@ -1,8 +1,27 @@
 """
 conway.py
-Author: <your name here>
+Author: Emma Tysinger
 Credit: <list sources used, if any>
 Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
+
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+from ggame import Color, Sound, SoundAsset
+
+myapp = App()
+
+# Background
+black = Color(0, 1)
+noline = LineStyle(0, black)
+bg_asset = RectangleAsset(myapp.width, myapp.height, noline, black)
+bg = Sprite(bg_asset, (0,0))
+
+myapp.run()
+
+class grid(App):
+    def __init__(self,rows,columns):
+        super().__init__()
+        self.rows=rows
+        self.columns=columns
