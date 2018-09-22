786d85fcd57b3491548dc553089a7f76fb566b15
emmadunbar <42850826+emmadunbar@users.noreply.github.com>
2018-09-22 18:31:06 -0400

    Updated from Brython Server: 9/22/2018 6:31:05 PM

--- conway.py
+++ conway.py
@@ -9,7 +9,7 @@ https://github.com/HHS-IntroProgramming/Conway-Life
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
 xsize=10
-ysize=10
+ysize=20
 pixelsize=6
 
 green=Color(0x00ff00, 1.0)
@@ -29,10 +29,14 @@ for i in range(0,n):
     i=none
     l1=l1+[i]
 
-for j in range(0,len(l1)):
-    print(j)
+def nei(r):
+    if r==0 or r==xsize-1 or r==(xsize*ysize-1) or r==(xsize*ysize-xsize):
+        d=3
+    else:
+        
     
-
+#for j in range(0,len(l1)):
+    #nei(j)
 
 def createlife():
     Sprite(life1,(x*pixelsize,y*pixelsize))
