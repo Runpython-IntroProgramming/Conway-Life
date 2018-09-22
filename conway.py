924c275cdcc89429407b55c704eabdee6e948e5a
emmadunbar <42850826+emmadunbar@users.noreply.github.com>
2018-09-22 17:50:12 -0400

    Updated from Brython Server: 9/22/2018 5:50:11 PM

--- conway.py
+++ conway.py
@@ -8,18 +8,22 @@ https://github.com/HHS-IntroProgramming/Conway-Life
 """
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
+xsize=50
+ysize=50
+pixelsize=6
+
 green=Color(0x00ff00, 1.0)
 blue=Color(0x0000ff, 1.0)
 white=Color(0xffffff, 1.0)
 black=Color(0x000000, 1.0)
 tline=LineStyle(0, black)
 
-life1=(RectangleAsset(6, 6, tline, green))
-life2=(RectangleAsset(6, 6, tline, blue))
-none=(RectangleAsset(6, 6, tline, white))
+life1=(RectangleAsset(pixelsize, pixelsize, tline, green))
+life2=(RectangleAsset(pixelsize, pixelsize, tline, blue))
+none=(RectangleAsset(pixelsize, pixelsize, tline, white))
 
 l1=[]
-n=2500
+n=xsize*ysize
 
 for i in range(0,n):
     i=none
@@ -27,16 +31,16 @@ for i in range(0,n):
 
 
 def createlife():
-    Sprite(life1,(x,y))
+    Sprite(life1,(x*pixelsize,y*pixelsize))
     return
 def staylife():
-    Sprite(life2,(x,y))
+    Sprite(life2,(x*pixelsize,y*pixelsize))
     return
 def death():
-    Sprite(none,(x,y))
+    Sprite(none,(x*pixelsize,y*pixelsize))
 
-for x in range(0,300,6):
-    for y in range(0,300,6):
+for x in range(0,xsize):
+    for y in range(0,ysize):
         death()
 
 
