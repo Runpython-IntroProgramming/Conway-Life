6b9d67e17b7fc4b54643a000479326e80f984b0a
chenmasterandrew <30731383+chenmasterandrew@users.noreply.github.com>
2018-09-24 15:55:27 -0400

    Updated from Brython Server: 9/24/2018 3:55:24 PM

--- conway.py
+++ conway.py
@@ -3,6 +3,7 @@ conway.py
 Author: Andrew Chen
 Credit:
 https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+http://brythonserver.github.io/ggame/
 
 Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
@@ -25,5 +26,42 @@ Your live cells should be two different colors: one for its first day of “life
 3. utilize reference system each tick to check every point and its neighbors simultaneously
 4. actvate changes and restart
 """
+from ggame import App, Color, LineStyle, Sprite, RectangleAsset
+black = Color(0x000000, 1.0)
+darkgrey = Color(0x2C3E40, 1.0)
+lightgrey = Color(0xEAECEE, 1.0)
+white = Color(0xffffff, 1.0)
+line = LineStyle(1, black)
 
-print
+deadcell = RectangleAsset(10, 10, line, darkgrey)
+newcell = RectangleAsset(10, 10, line, white)
+oldcell = RectangleAsset(10, 10, line, lightgrey)
+
+coordinates = {}
+for x in range(10):
+    for y in range(10):
+        coordinates[(x,y)] = "G"
+        print(coordinates[(x,y)] , end = "")
+    else:
+        print("")
+
+
+
+
+
+
+
+Sprite(cell,(10,10))
+Sprite(cell,(10,0))
+
+
+
+
+
+
+
+
+app = App()
+app.run()
+
+    
