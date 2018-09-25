2a0c688cfe34a25a187df8b2c2b3b0c44779504f
chenmasterandrew <30731383+chenmasterandrew@users.noreply.github.com>
2018-09-25 17:30:06 -0400

    Updated from Brython Server: 9/25/2018 5:30:05 PM

--- conway.py
+++ conway.py
@@ -4,6 +4,8 @@ Author: Andrew Chen
 Credit:
 https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
 http://brythonserver.github.io/ggame/
+https://github.com/HHS-IntroProgramming/Standards-and-Syllabus/wiki/Advanced-Graphics-with-Classes
+https://github.com/HHS-IntroProgramming/Standards-and-Syllabus/wiki/Dictionaries-and-Classes
 
 Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
@@ -26,24 +28,37 @@ Your live cells should be two different colors: one for its first day of “life
 3. utilize reference system each tick to check every point and its neighbors simultaneously
 4. actvate changes and restart
 """
+
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, KeyEvent, MouseEvent
 black = Color(0x000000, 1.0)
-darkgrey = Color(0x2C3E40, 1.0)
-lightgrey = Color(0x808080, 1.0)
+grey = Color(0x808080, 1.0)
 white = Color(0xffffff, 1.0)
 line = LineStyle(1, black)
 
 deadcell = RectangleAsset(10, 10, line, black)
 newcell = RectangleAsset(10, 10, line, white)
-oldcell = RectangleAsset(10, 10, line, lightgrey)
+oldcell = RectangleAsset(10, 10, line, grey)
 
+print("""==================================================
+CONTROLS
+Click anywhere on the coordinate plane to kill or birth a cell
+Press "space" to advance time one frame
+==================================================
+RULES
+1. Any live cell with fewer than two live neighbors dies, as if by under population.
+2. Any live cell with two or three live neighbors lives on to the next generation.
+3. Any live cell with more than three live neighbors dies, as if by overpopulation.
+4. Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+==================================================""")
+
+#mapsize = int(input("Please input how long the map should be: ")) + 1
 mapsize = 10
 
 # Creates coordinate plane and sets all cells to dead
 coord = {}
 for x in range(mapsize+1):
     for y in range(mapsize+1):
-        coord[(x,y)] = 1
+        coord[(x,y)] = 0
 
 # Function that displays cell grid
 def display():
@@ -51,13 +66,13 @@ def display():
         for y in range(1,mapsize):
             if coord[(x,y)] == 0:
                 Sprite(deadcell,(x*10,y*10))
-            if coord[(x,y)] == 1:
+            elif coord[(x,y)] == 1:
                 Sprite(newcell,(x*10,y*10))
-            if coord[(x,y)] == 2:
+            elif coord[(x,y)] == 2:
                 Sprite(oldcell,(x*10,y*10))
 
 # Function that checks and compiles cells to change
-def ticker():
+def ticker(test):
     birthcell = []
     agecell = []
     killcell = []
@@ -65,16 +80,17 @@ def ticker():
         for y in range(1,mapsize):
             
             liveneigh = 0
-            for xneigh in range(x-1,x+1):
-                for yneigh in range(y-1,y+1):
-                    if coord[(xneigh,yneigh)] != 0:
+            for xneigh in range(x-1,x+2):
+                for yneigh in range(y-1,y+2):
+                    if coord[(xneigh,yneigh)] == 1 or coord[(xneigh,yneigh)] == 2:
                         liveneigh += 1
             if coord[(x,y)] == 0 and liveneigh == 3:
                 birthcell.append((x,y))
-            elif coord[(x,y)] == 1 and liveneigh == 2  or liveneigh == 3:
-                agecell.append((x,y))
-            elif coord[(x,y)] != 0 and liveneigh != 2  or liveneigh != 3:
-                killcell.append((x,y))
+            elif coord[(x,y)] == 1 or coord[(x,y)] == 2:
+                if liveneigh == 3 or liveneigh == 4:
+                    agecell.append((x,y))
+                else:
+                    killcell.append((x,y))
     for birth in birthcell:
         coord[birth] = 1
     for age in agecell:
@@ -84,17 +100,15 @@ def ticker():
     display():
 
 def change(info):
-    if coord[(int(info.x/10),int(info.y/10))] == 0:
-        coord[(int(info.x/10),int(info.y/10))] = 1
     if coord[(int(info.x/10),int(info.y/10))] == 1 or coord[(int(info.x/10),int(info.y/10))] == 2:
         coord[(int(info.x/10),int(info.y/10))] = 0
+    elif coord[(int(info.x/10),int(info.y/10))] == 0:
+        coord[(int(info.x/10),int(info.y/10))] = 1
     display()
-
-def step(test):
-    ticker()
         
 display():
-App.listenKeyEvent("keydown", "space", step)
+
+App.listenKeyEvent("keydown", "space", ticker)
 App.listenMouseEvent("mousedown", change)
 
 app = App()
