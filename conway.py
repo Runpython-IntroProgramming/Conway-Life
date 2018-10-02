55c6b442d3ae37af2906c07f828e67626db4eedf
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-10-02 13:04:29 -0400

    Updated from Brython Server: 10/2/2018 1:04:28 PM

--- conway.py
+++ conway.py
@@ -9,18 +9,18 @@ https://github.com/HHS-IntroProgramming/Conway-Life
 from ggame import App, Color, LineStyle, Sprite
 from ggame import RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
-# Three primary colors with no transparency (alpha = 1.0)
+
 red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
+#colors
 
-# Define a line style that is a thin (1 pixel) wide black line
 thinline = LineStyle(1, black)
-# A graphics asset that represents a rectangle
+#lines
 rectangle = RectangleAsset(50, 20, thinline, blue)
 
-# Now display a rectangle
+
 Sprite(rectangle)
 
 myapp = App()
