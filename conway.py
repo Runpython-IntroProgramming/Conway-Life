af294b2d32eb32de40ee13e0704eabee452e8988
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-10-02 13:04:00 -0400

    Updated from Brython Server: 10/2/2018 1:03:59 PM

--- conway.py
+++ conway.py
@@ -6,3 +6,22 @@ Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
+from ggame import App, Color, LineStyle, Sprite
+from ggame import RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
+
+# Three primary colors with no transparency (alpha = 1.0)
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+
+# Define a line style that is a thin (1 pixel) wide black line
+thinline = LineStyle(1, black)
+# A graphics asset that represents a rectangle
+rectangle = RectangleAsset(50, 20, thinline, blue)
+
+# Now display a rectangle
+Sprite(rectangle)
+
+myapp = App()
+myapp.run()
