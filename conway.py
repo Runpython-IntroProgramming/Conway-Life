5982df01cec7499abadccca60dd1ffb1feda7314
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-10-03 11:58:14 -0400

    Updated from Brython Server: 10/3/2018 11:58:13 AM

--- conway.py
+++ conway.py
@@ -6,6 +6,15 @@ Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
+"""
+You may use code supplied in the graphics tutorials as a starting point.
+Live cells in your program may be represented as rectangles, circles, or any other shape that you would like to use. The example in the video above uses small circles.
+The initial state of live cells may be preset or randomized by you, but it must be possible to start the game with a blank screen.
+As the Wikipedia article described, your "playing area" may have fixed boundaries, boundaries that wrap around top and bottom, or may be entirely unbounded (in some ways, the easiest approach!).
+The user must be able to "turn on" cells by clicking on them with the mouse, or by click-dragging across the window.
+If your playing area is unbounded, then the up/down/right/left cursor keys should allow the user to scroll the playing area within the window.
+Your live cells should be two different colors: one for its first day of “life”, the second for all subsequent days.
+"""
 from ggame import App, Color, LineStyle, Sprite
 from ggame import RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
@@ -19,6 +28,7 @@ black = Color(0x000000, 1.0)
 thinline = LineStyle(1, black)
 #lines
 rectangle = RectangleAsset(50, 20, thinline, blue)
+
 
 
 Sprite(rectangle)
