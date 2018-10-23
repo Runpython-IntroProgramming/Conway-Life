ffc3d48cd35659de8453320f452557a6c97f96c9
JacksonTolliday <42842756+JacksonTolliday@users.noreply.github.com>
2018-10-23 11:57:03 -0400

    Updated from Brython Server: 10/23/2018 11:57:03 AM

--- conway.py
+++ conway.py
@@ -5,7 +5,13 @@ Credit: <list sources used, if any>
 Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
+        RULES:
+    Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+    Any live cell with two or three live neighbors lives on to the next generation.
+    Any live cell with more than three live neighbors dies, as if by overpopulation.
+    Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
 """
+
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset
 
 day1 = Color(0xffffff, 1.0)
@@ -13,3 +19,55 @@ day2 = Color(0x000000, 1.0)
 grey = Color(0xC0C0C0, 1.0)
 gridgrey = LineStyle(1, grey)
 noline = LineStyle(0, grey)
+
+class Cell(Sprite):
+    """
+    Cell outline
+    """
+    asset = RectangleAsset("images/four_spaceship_by_albertov_with_thrust.png",
+        Frame(227,0,65,125), 4, 'vertical')
+
+    def __init__(self, position):
+        super().__init__(SpaceShip.asset, position)
+        SpaceGame.listenMouseEvent("click", "mouseclick", self.clone)
+        self.fxcenter = self.fycenter = 0.5
+        self.thrust = 0
+        self.thrustframe = 1
+
+    def step(self):
+        '''self.x += self.vx'''
+        self.y += -self.thrust
+        self.rotation += self.vr
+        # manage thrust animation
+        if self.thrust == 1:
+            print('step')
+            self.setImage(self.thrustframe)
+            self.thrustframe += 1
+            if self.thrustframe == 4:
+                self.thrustframe = 1
+        else:
+            self.setImage(0)
+
+    def mouseClick(event):
+        ball.x = event.x
+        ball.y = event.y
+
+
+class ConwayLife(App):
+    """
+    Game setup
+    """
+    def __init__(self):
+        super().__init__()
+        # Background
+        bg_asset = RectangleAsset(self.width, self.height, noline, grey)
+        bg = Sprite(bg_asset, (0,0))
+
+
+    def step(self):
+        for ship in self.getSpritesbyClass(SpaceShip):
+            ship.step()
+
+
+myapp = SpaceGame()
+myapp.run()
