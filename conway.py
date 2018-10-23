60dfa15e797ffdcc22dea19e318baa5f2e2316b4
JacksonTolliday <42842756+JacksonTolliday@users.noreply.github.com>
2018-10-23 11:40:36 -0400

    Updated from Brython Server: 10/23/2018 11:40:35 AM

--- conway.py
+++ conway.py
@@ -6,3 +6,10 @@ Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
+from ggame import App, Color, LineStyle, Sprite, RectangleAsset
+
+day1 = Color(0xffffff, 1.0)
+day2 = Color(0x000000, 1.0)
+grey = Color(0xC0C0C0, 1.0)
+gridgrey = LineStyle(1, grey)
+noline = LineStyle(0, grey)
