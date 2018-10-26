800d09b0ffcc9bcc16a6263167c67b3733aa52e3
ericgoodney <42844640+ericgoodney@users.noreply.github.com>
2018-10-26 12:47:53 -0400

    Updated from Brython Server: 10/26/2018 12:47:53 PM

--- conway.py
+++ conway.py
@@ -6,3 +6,14 @@ Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+
+myapp = App()
+
+# Background
+black = Color(0, 1)
+noline = LineStyle(0, black)
+bg_asset = RectangleAsset(myapp.width, myapp.height, noline, black)
+bg = Sprite(bg_asset, (0,0))
+
+myapp.run()
