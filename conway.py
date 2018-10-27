2a8a142fdd3571209dcac210fefd905c186f618c
ericgoodney <42844640+ericgoodney@users.noreply.github.com>
2018-10-26 21:51:30 -0400

    Updated from Brython Server: 10/26/2018 9:51:29 PM

--- conway.py
+++ conway.py
@@ -15,10 +15,12 @@ class SpaceGame(App):
     Tutorial4 space game example.
     """
     def __init__(self):
-        super(black = Color(0, 1)
+        super().__init__()
+        # Background
+        black = Color(0, 1)
         noline = LineStyle(0, black)
         bg_asset = RectangleAsset(self.width, self.height, noline, black)
-        bg = Sprite(bg_asset, (0,0))).__init__()
+        bg = Sprite(bg_asset, (0,0))
 
 myapp.run()
 
