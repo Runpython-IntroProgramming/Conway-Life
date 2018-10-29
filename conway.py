cf95cffd5716501fbb45b582fee996e64e1095d3
ericgoodney <42844640+ericgoodney@users.noreply.github.com>
2018-10-29 13:05:29 -0400

    Updated from Brython Server: 10/29/2018 1:05:29 PM

--- conway.py
+++ conway.py
@@ -23,7 +23,22 @@ class SpaceGame(App):
 
 myapp = SpaceGame()
 myapp.run()
+"""
+Rules:
+Player makes initial layout
+Ways for game to end: all cells die,
+no cells change from one generation to the next, 
+or the pattern flips back and forth between two or more positions.
 
+How cells work:
+Births: Each dead cell adjacent to exactly three live neighbors will become live in the next generation.
+Death by isolation: Each live cell with one or fewer live neighbors will die in the next generation.
+Death by overcrowding: Each live cell with four or more live neighbors will die in the next generation.
+Survival: Each live cell with either two or three live neighbors will remain alive for the next generation.
+
+Corners Do Not Count 
+
+"""
 
 
 
