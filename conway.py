37012247188812414680544c8d393330c68d2b9c
JacksonTolliday <42842756+JacksonTolliday@users.noreply.github.com>
2018-10-31 11:31:32 -0400

    Updated from Brython Server: 10/31/2018 11:31:32 AM

--- conway.py
+++ conway.py
@@ -16,12 +16,27 @@ from ggame import App, Color, LineStyle, Sprite, RectangleAsset
 
 day1 = Color(0xffff00, 1.0)
 day2 = Color(0xffffff, 1.0)
-grey = Color(0xffffff, 1.0)
+grey = Color(0xffffff, 1.0) 
+trial = Color(0xC0C0C0, 1.0)
 gridgrey = LineStyle(1, grey)
 noline = LineStyle(0, grey)
 
 class PosCell(Sprite):
-    
+    """
+    Cell outline
+    """
+    asset = RectangleAsset(11, 11, gridgrey, trial)
+
+    def __init__(self, position):
+        super().__init__(Cell.asset, position)
+        self.fxcenter = self.fycenter = 0.5
+        self.createlist = []
+
+    def step(self):
+        if len(list(self.collidingWithSprites(Cell))) == 3:
+            self.createlist.append(cell)
+            print(self.createlist)
+
 
 class Cell(Sprite):
     """
@@ -32,17 +47,6 @@ class Cell(Sprite):
     def __init__(self, position):
         super().__init__(Cell.asset, position)
         self.fxcenter = self.fycenter = 0.5
-
-    def alive():
-        pass
-    #Any live cell with two or three live neighbors lives on to the next generation.
-    #Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
-    
-    def dead():
-        pass
-    #Any live cell with fewer than two live neighbors dies, as if by underpopulation.
-    #Any live cell with more than three live neighbors dies, as if by overpopulation.
-    #if neighbors > 3 or neighbors < 1:
 
     def step(self):
         print(len(list(self.collidingWithSprites(Cell))))
@@ -67,6 +71,8 @@ class ConwayLife(App):
         print(self.a)
         for cell in self.getSpritesbyClass(Cell):
             cell.step()
+        for sell in self.getSpritesbyClass(PosCell):
+            sell.step
 
     def mouseClick(self, event):
         if ((((round(event.x/10))*10),((round(event.y/10))*10))) not in self.a:
