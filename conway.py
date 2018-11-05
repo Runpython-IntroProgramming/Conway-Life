7ad56927e6b8c3fb47a4d0e2118106c94d31bcb4
ericgoodney <42844640+ericgoodney@users.noreply.github.com>
2018-11-05 12:57:54 -0500

    Updated from Brython Server: 11/5/2018 12:57:52 PM

--- conway.py
+++ conway.py
@@ -55,7 +55,7 @@ for x in range(11):
     Sprite(line2, (0, x*40))
 
 #Making user be able to click and fill a cell
-cell_asset = RectangleAsset(40,40,thinline, white)
+cell_asset = RectangleAsset(40,40,thinline, orange)
 def mouseClick(event):
 #making a rectangle in closest box
     x = floor(event.x/40)*40 
