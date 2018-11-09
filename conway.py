1848636416881de750c818ce0bf86c45fe41d421
JacksonTolliday <42842756+JacksonTolliday@users.noreply.github.com>
2018-11-09 12:10:21 -0500

    Updated from Brython Server: 11/9/2018 12:10:20 PM

--- conway.py
+++ conway.py
@@ -105,15 +105,15 @@ class ConwayLife(App):
                 Poscell.posposcells.append((cell[0]+10, cell[1]-10))
                 Poscell.posposcells = sorted(Poscell.posposcells)
                 print(Poscell.posposcells)
-                for countcopies in Poscell.posposcells:
-                    copies = Poscell.posposcells.count(countcopies)
-                    if copies >= 0:
-                        pass
-                for nposcellk, nposcelld in self.celldict.items():
-                    if nposcellk in Poscell.posposcells:
-                        pass
-                        
-                        
+        for countcopies in Poscell.posposcells:
+            copies = Poscell.posposcells.count(countcopies)
+            if copies >= 0:
+                pass
+        for nposcellk, nposcelld in self.celldict.items():
+            if nposcellk in Poscell.posposcells:
+                pass
+                
+                
     def ps(self, event):
         print(self.shredlist)
 
