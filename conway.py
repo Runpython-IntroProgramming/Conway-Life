a95ed33665822f399acf82df8241943a7b97fccc
ellaedmonds <35499735+ellaedmonds@users.noreply.github.com>
2018-11-25 22:01:11 -0500

    Updated from Brython Server: 11/25/2018 10:01:11 PM

--- conway.py
+++ conway.py
@@ -87,10 +87,12 @@ class Game(App):
     Cells = []
     Sprites = []
     madecells = []
+    madecellslist = []
     
     for m in d:
         cells[(m[0],m[1])] = "dead"
         sprites[(m[0],m[1])] = DeadCell
+        madecellslist.append((sprites.get((m[0],m[1])),((m[0],m[1]))))
         madecells.append(sprites.get((m[0],m[1]))((m[0],m[1])))
         Cells.append((m[0],m[1]))
     
@@ -104,20 +106,27 @@ class Game(App):
                 if m[1] <= event.y <= m[1]+8:
                     cells[(m[0],m[1])] = "baby"
                     sprites[(m[0],m[1])] = BabyCell
-                    #BabyCell((m[0],m[1]))
+                    BabyCell((m[0],m[1]))
             else:
                 cells[(m[0],m[1])] = "nothing"
 
-    def step(self):
+    def clear(self,event):
+        b=0
         for n in self.madecells:
+            B = self.madecellslist[b]
+            if B[0] == "baby":
+                cells[sprints[B[0]]] = "old"
             n.destroy()
+            b+=1
         self.madecells = []
+        longtermcells = []
         for m in self.Cells:
             self.madecells.append(sprites.get((m[0],m[1]))((m[0],m[1])))
+            sprites.get((m[0],m[1]))((m[0],m[1]))
         print(self.Cells)
+        
             
-    def clear(self,event):
-        print("hi")
+
         
 
 
