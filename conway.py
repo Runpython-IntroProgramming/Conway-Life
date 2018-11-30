6f470fc899e799e15822bc6dfe90f905b5af70d1
ericgoodney <42844640+ericgoodney@users.noreply.github.com>
2018-11-30 12:48:19 -0500

    Updated from Brython Server: 11/30/2018 12:48:19 PM

--- conway.py
+++ conway.py
@@ -27,6 +27,7 @@ for x in range(11):
     Sprite(line1, (((x*40)+320), 120))
     Sprite(line2, (320, ((x*40)+120)))
 ball_asset = ImageAsset("Conway-life.png", Frame(62,210,275,275),3)
+#blue=image(0) [dead]  red=image(1)[baby] black=image(2)[old]
 #have beginning of the games start out with 
 sprites = {}
 for g in range (10):
@@ -41,52 +42,69 @@ for g in range (10):
 def mouseClick(event):
     x = floor(event.x/40)*40
     y = floor(event.y/40)*40
-    s = sprites[(x,y)]
+    s = sprites[(x,y)]  
     s.setImage(1)
+    
+
 #Getting into the actual conway game of life
 #Finding which neighboring cells are alive or dead. 
 def livingneighbors(position):
     neighbors = 0
-    if sprites[(position[0]+40,position[1])].index ==1:
+    if sprites[(position[0]+40,position[1])].index in [0,2]:
         neighbors += 1
         #right
-    if sprites[(position[0]+40,position[1]+40)].index ==1:
+    if sprites[(position[0]+40,position[1]+40)].index in [0,2]:
         neighbors += 1
         #bottom right
-    if sprites[(position[0]+40,position[1]-40)].index ==1:
+    if sprites[(position[0]+40,position[1]-40)].index in [0,2]:
         neighbors += 1
         #top right
-    if sprites[(position[0]-40,position[1])].index ==1:
+    if sprites[(position[0]-40,position[1])].index in [0,2]:
         neighbors += 1
         #left
-    if sprites[(position[0]-40,position[1]-40)].index ==1:
+    if sprites[(position[0]-40,position[1]-40)].index in [0,2]:
         neighbors += 1
         #top left
-    if sprites[(position[0]-40,position[1]+40)].index ==1:
+    if sprites[(position[0]-40,position[1]+40)].index in [0,2]:
         neighbors += 1
         #bottom left 
-    if sprites[(position[0],position[1]+40)].index ==1:
+    if sprites[(position[0],position[1]+40)].index in [0,2]:
         neighbors += 1
         #bottom
-    if sprites[(position[0],position[1]-40)].index ==1:
+    if sprites[(position[0],position[1]-40)].index in [0,2]:
         neighbors += 1
         #top
     return neighbors
 
 spritestokill = []
+spritestobirth = []
 
 for pos in sprites:
+    """
     if pos.position.x > 320 and pos.position.x < 680 and pos.position.y < 480 and pos.position.y > 120:
-    #print noting 
-        if pos > (680,480)
         #alive = livingcell(pos)
         n = livingneighbors(pos)
+    """
         if n < 2 or n > 3:
             spritestokill.append(pos)
-            s = sprites[(x,y)]
-            s.setImage(0)
+        if n == 2:
+            spritestobirth.append(pos)
+        if n ==3:
+            spritestobirth.append(pos)
+
+for pos in spritestokill:
+    # kill this sprite
+    s = sprites[pos]
+    s.setImage(2)
+
+for pos in spritestobirth:
+    # set this sprite to born
+    s = sprites[pos]
+    s.setImage(1)
+    
         
         # do something to make the sprite at pos be DEAD
+
 """
 def living neigbors (position):
 neighbors = 0
