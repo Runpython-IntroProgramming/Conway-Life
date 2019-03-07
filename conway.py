07f04cd64e179a8aac1a1834aa8ef99c79153625
sth23 <47009471+sth23@users.noreply.github.com>
2019-03-07 15:06:27 -0500

    Updated from Brython Server: 3/7/2019 3:06:28 PM

--- conway.py
+++ conway.py
@@ -1,8 +1,52 @@
 """
 conway.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Sean Healey
+Credit: Tutorial, Stack Overflow
 Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
+from ggame import App, Color, LineStyle, Sprite, CircleAsset
+
+# Colors
+red = Color(0xff0000, 1.0)
+orange = Color(0xffa500, 1.0)
+yellow = Color(0xffff00, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+purple = Color(0x800080, 1.0)
+black = Color(0x000000, 1.0)
+white = Color(0xffffff, 1.0)
+
+noline = LineStyle(
+
+colorslist = [red, orange, yellow, green, blue, purple, black]
+
+class Cell(Sprite):
+    """"
+    Cell
+    """"
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.alive = 1
+        self.age = 0
+        self.color = colorslist[self.age]
+        
+        # Display Cell
+        if self.alive == 1:
+            circle = CircleAsset(5, noline, self.color)
+
+class Conway(App):
+    """"
+    Play Conway's Game
+    """"
+    
+    def __init__(self):
+        super().__init__()
+        
+        Cell()
+
+myapp = Conway()
+myapp.run()
