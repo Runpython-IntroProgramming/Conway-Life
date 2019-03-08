f8a7d1a4e4d9ef76dc2421501506bd023f9a3a08
sth23 <47009471+sth23@users.noreply.github.com>
2019-03-08 14:48:18 -0500

    Updated from Brython Server: 3/8/2019 2:48:20 PM

--- conway.py
+++ conway.py
@@ -51,7 +51,7 @@ print(grid)
 
 for i in grid:
     for j in i:
-        Sprite(circles[j]), (len(grid)*10,len(i)*10)
+        Sprite(circles[j%7]), (len(grid)*10,len(i)*10)
 
 myapp = App()
 myapp.run()
