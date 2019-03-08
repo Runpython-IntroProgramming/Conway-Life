b09724852ee3026b183d97ca9d8913f8c1521e7e
sth23 <47009471+sth23@users.noreply.github.com>
2019-03-08 16:15:54 -0500

    Updated from Brython Server: 3/8/2019 4:15:56 PM

--- conway.py
+++ conway.py
@@ -35,8 +35,8 @@ whitecircle = CircleAsset(5, noline, white)
 circles = [whitecircle, redcircle, orangecircle, yellowcircle, greencircle, bluecircle, purplecircle, blackcircle]
 
 # Get dimensions from user
-gridcolumns = 5
-gridrows = 5
+gridcolumns = 3
+gridrows = 3
 grid = []
 
 # Create grid
@@ -67,18 +67,22 @@ print(grid)
 # Need to make old grid and new grid
 # Need to include conditional statemens incase i < 0 or j < 0
 newgrid = grid
-for i in range(1, len(grid)-2):
-    for j in range(1, len(grid[0])-2):
+for i in range(0, len(grid)):
+    for j in range(0, len(grid[0])):
         neighbors = 0
-        neighbors += (grid[i-1][j-1] != 0)
-        neighbors += (grid[i-1][j] != 0)
-        neighbors += (grid[i-1][j+1] != 0)
+        if i != 0 & j != 0:
+            neighbors += (grid[i-1][j-1] != 0)
+        if i != 0:
+            neighbors += (grid[i-1][j] != 0)
+        if i != 0 & j != len(grid[0]):
+            neighbors += (grid[i-1][j+1] != 0)
         neighbors += (grid[i][j-1] != 0)
         neighbors += (grid[i][j] != 0)
         neighbors += (grid[i][j+1] != 0)
         neighbors += (grid[i+1][j-1] != 0)
         neighbors += (grid[i+1][j] != 0)
         neighbors += (grid[i+1][j+1] != 0)
+        print(neighbors)
         if neighbors < 2:
             newgrid[i][j] = 0
         elif neighbors > 3:
@@ -86,7 +90,7 @@ for i in range(1, len(grid)-2):
         else:
             newgrid[i][j] = grid[i][j] + 1
         
-print(grid)
+print(newgrid)
         
     
 #myapp = App()
