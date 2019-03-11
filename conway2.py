3e5c2291731be53a4e543bf159ef5b2ccda5fa92
sth23 <47009471+sth23@users.noreply.github.com>
2019-03-11 09:23:39 -0400

    Updated from Brython Server: 3/11/2019 9:23:40 AM

--- conway2.py
+++ conway2.py
@@ -56,62 +56,61 @@ for i in range(0,gridrows):
 def tick():            
     # Keeps running simulation until reaching desired number of generations
     # Make this into a function
-    #gen_count = 0
-    #while gen_count < num_generations:
-    # Clears grid
-    Sprite(RectangleAsset(gridrows*10, gridcolumns*10, noline, white), (0,0))
-    
-    # Create tick function
-    # Neighbors matrix counts neighbors for each cell (need to convert this into function)
-    neighbors = [0] * len(grid)
-    neighbors = [ [0] * len(grid[0]) for x in neighbors]
-
-    for i in range(0, len(grid)): # rows
-        for j in range(0, len(grid[0])): # columns
-            count = 0
-            if i-1 >= 0 and j-1 >= 0 and grid[i-1][j-1] != 0:
-                count += 1
-            if i-1 >= 0 and grid[i-1][j] != 0:
-                count += 1
-            if i-1 >= 0 and j+1 < len(grid[0]) and grid[i-1][j+1] != 0:
-                count += 1
-            if j-1 >= 0 and grid[i][j-1] != 0:
-                count += 1
-            if j+1 < len(grid[0]) and grid[i][j+1] != 0:
-                count += 1
-            if i+1 < len(grid) and j-1 >= 0 and grid[i+1][j-1] != 0:
-                count += 1
-            if i+1 < len(grid) and grid[i+1][j] != 0:
-                count += 1
-            if i+1 < len(grid) and j+1 < len(grid[0]) and grid[i+1][j+1] != 0:
-                count += 1
-            neighbors[i][j] = count
+    gen_count = 0
+    while gen_count < num_generations:
+        # Clears grid
+        Sprite(RectangleAsset(gridrows*10, gridcolumns*10, noline, white), (0,0))
         
-    for i in range(0, len(grid)):
-        for j in range(0,len(grid[0])):
-            if neighbors[i][j] < 2:
-                grid[i][j] = 0
-            elif neighbors[i][j] > 3:
-                grid[i][j] = 0
-            elif grid[i][j] != 0:
-                grid[i][j] += 1
-            elif neighbors[i][j] == 3:
-                grid[i][j] += 1
-    gen_count += 1
+        # Create tick function
+        # Neighbors matrix counts neighbors for each cell (need to convert this into function)
+        neighbors = [0] * len(grid)
+        neighbors = [ [0] * len(grid[0]) for x in neighbors]
     
-
-    # Make this into it's own function
-    # Create Sprites for each element in grid
-    x = 0
-    y = 0
-    for i in grid:
-        for j in i:
-            if j > 7:
-                Sprite(circles[6], (x,y))
-            elif j > 0:
-                Sprite(circles[j-1], (x,y))
-            x += 10
+        for i in range(0, len(grid)): # rows
+            for j in range(0, len(grid[0])): # columns
+                count = 0
+                if i-1 >= 0 and j-1 >= 0 and grid[i-1][j-1] != 0:
+                    count += 1
+                if i-1 >= 0 and grid[i-1][j] != 0:
+                    count += 1
+                if i-1 >= 0 and j+1 < len(grid[0]) and grid[i-1][j+1] != 0:
+                    count += 1
+                if j-1 >= 0 and grid[i][j-1] != 0:
+                    count += 1
+                if j+1 < len(grid[0]) and grid[i][j+1] != 0:
+                    count += 1
+                if i+1 < len(grid) and j-1 >= 0 and grid[i+1][j-1] != 0:
+                    count += 1
+                if i+1 < len(grid) and grid[i+1][j] != 0:
+                    count += 1
+                if i+1 < len(grid) and j+1 < len(grid[0]) and grid[i+1][j+1] != 0:
+                    count += 1
+                neighbors[i][j] = count
+            
+        for i in range(0, len(grid)):
+            for j in range(0,len(grid[0])):
+                if neighbors[i][j] < 2:
+                    grid[i][j] = 0
+                elif neighbors[i][j] > 3:
+                    grid[i][j] = 0
+                elif grid[i][j] != 0:
+                    grid[i][j] += 1
+                elif neighbors[i][j] == 3:
+                    grid[i][j] += 1
+        gen_count += 1
+        
+        # Make this into it's own function
+        # Create Sprites for each element in grid
         x = 0
-        y += 10
+        y = 0
+        for i in grid:
+            for j in i:
+                if j > 7:
+                    Sprite(circles[6], (x,y))
+                elif j > 0:
+                    Sprite(circles[j-1], (x,y))
+                x += 10
+            x = 0
+            y += 10
                     
 myapp.run(tick)
